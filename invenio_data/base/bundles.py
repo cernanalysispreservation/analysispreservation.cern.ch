0dbccbb684ddfce24e9de413691c859c3ba86046
Marco Neumann <marco@crepererum.net>
2015-08-27 16:43:31 +0200

    global: self-hosting of used fonts
    * Moves from Google Fonts to self-hosted font data. This prevents our
      users from tracking. This also adds the missing font data for "PT Sans
      Bold" and (in case it is used somewhere on our site) italic versions.
    
    Signed-off-by: Marco Neumann <marco@crepererum.net>

--- invenio_data/base/bundles.py
+++ invenio_data/base/bundles.py
@@ -25,3 +25,4 @@ from invenio.base.bundles import styles as _styles
 _styles.contents.remove("less/base.less")
 _styles.contents += ("less/cds.less",)
 _styles.contents += ("less/experiments.less",)
+_styles.contents += ("less/font.less",)
