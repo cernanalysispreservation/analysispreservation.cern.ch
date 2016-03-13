9b9d2d4d6bc05cd16b178919cba84d879f304505
Pamfilos Fokianos <pamfilosf@gmail.com>
2016-03-13 23:40:54 +0100

    ATLAS: updates Analysis Workflows
    * updates basic schema
    * adds options

--- cap/modules/records/jsonschemas/options/ATLASWorkflows.js
+++ cap/modules/records/jsonschemas/options/ATLASWorkflows.js
@@ -2,94 +2,115 @@ var subschema_option_fields = {};
 
 window.schemaOptions = {
   "fields": {
-    "context": {
-      "type": "depositgroup"
+    "basic_info": {
+      "type": "depositgroup",
+      "fields": {
+      }
     },
-    "stages": {
+    "workflows": {
       "type": "depositgroup-array",
       "fields": {
         "item": {
           "fields": {
-            "name": {
-              "order": 1
+            "context": {
+              "type": "depositgroup-object"
             },
-            "dependencies": {
+            "stages": {
               "type": "depositgroup-object-array",
-              "order": 4,
               "fields": {
-                "items": {
+                "item": {
                   "fields": {
-                    "placeholder": "Add dependency",
-                    "type": "select",
-                    "multiple": true,
-                    "dataSource": function(callback){
-                      callback(_.values(dependencies));
+                    "name": {
+                      "order": 1
+                    },
+                    "dependencies": {
+                      "type": "depositgroup-object-array",
+                      "order": 4,
+                      "fields": {
+                        "items": {
+                          "fields": {
+                            "placeholder": "Add dependency",
+                            "type": "select",
+                            "multiple": true,
+                            "dataSource": function(callback){
+                              callback(_.values(dependencies));
+                            }
+                          }
+                        }
+                      }
+                    },
+                    "parameters": {
+                      "type": "depositgroup-object",
+                      "order": 3
+                    },
+                    "scheduler": {
+                      "order": 2,
+                      "fields": {
+                        "schema_type": {
+                          "hidden": "true"
+                        },
+                        "steps": {
+                          "fields": {
+                            "schema_type": {
+                              "hidden": "true"
+                            },
+                            "process": {
+                              "fields": {
+                                "process-type": {
+                                  "hidden": "true"
+                                }
+                              }
+                            },
+                            "publisher": {
+                              "schemaTypeField": "publisher_type",
+                              "fields": {
+                                "publisher_type": {
+                                  "hidden": "true"
+                                }
+                              }
+                            },
+                            "environment": {
+                              "fields": {
+                                "environment_type": {
+                                  "hidden": "true"
+                                },
+                                "image": {
+                                  "order" : 1
+                                },
+                                "imagetag": {
+                                  "order" : 2
+                                },
+                                "resources": {
+                                  "order" : 3,
+                                  "type": "depositgroup-object-array"
+                                },
+                                "envscript": {
+                                  "order" : 4
+                                },
+                                "envvars": {
+                                  "order" : 5
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
                     }
                   }
                 }
               }
             },
-            "parameters": {
-              "type": "depositgroup-object",
-              "order": 3
+            "processes": {
             },
-            "scheduler": {
-              "order": 2,
-              "fields": {
-                "steps": {
-                  "fields": {
-                    "process": {
-                      "fields": {
-                        "process-type": {
-                          "hidden": "true"
-                        }
-                      }
-                    },
-                    "publisher": {
-                      "fields": {
-                        "publisher-type": {
-                          "hidden": "true"
-                        }
-                      }
-                    },
-                    "environment": {
-                      "fields": {
-                        "environment-type": {
-                          "hidden": "true"
-                        },
-                        "image": {
-                          "order" : 1
-                        },
-                        "imagetag": {
-                          "order" : 2
-                        },
-                        "resources": {
-                          "order" : 3,
-                          "type": "depositgroup-object-array"
-                        },
-                        "envscript": {
-                          "order" : 4
-                        },
-                        "envvars": {
-                          "order" : 5
-                        }
-                      }
-                    }
-                  }
-                }
-              }
+            "environments": {
+              "type": "depositgroup-array",
+            },
+            "publishers": {
+              "type": "depositgroup-array",
             }
           }
         }
       }
-    },
-    "processes": {
-    },
-    "environments": {
-      "type": "depositgroup-array",
-    },
-    "publishers": {
-      "type": "depositgroup-array",
     }
   },
   "view": "invenio-view"
@@ -97,29 +118,30 @@ window.schemaOptions = {
 
 var dependencies = {};
 window.schemaPostRender = function(control){
-  // var root = control.children;
-  // var observableList = ["ntup", "download_nodes"];
+  var root = control.children;
+  window.rrrrrrrr = root;
+  var observableList = ["ntup", "download_nodes"];
 
-  // root = _.reject(root, function(r){
-  //   if (_.indexOf(observableList, r.name) > -1)
-  //     return false;
-  //   else
-  //     return true;
-  // });
+  root = _.reject(root, function(r){
+    if (_.indexOf(observableList, r.name) > -1)
+      return false;
+    else
+      return true;
+  });
 
-  // _.each(root, function(r){
-  //   r.on('change', function(){
-  //     var depsToAdd = {};
-  //     _.each(r.children, function(d){
-  //       var name = d.childrenByPropertyId['name'];
-  //       if (name.data) depsToAdd[d.id] = name.data;
-  //       // console.log("-----:", d.id , " == ", name.data);
+  _.each(root, function(r){
+    r.on('change', function(){
+      var depsToAdd = {};
+      _.each(r.children, function(d){
+        var name = d.childrenByPropertyId['name'];
+        if (name.data) depsToAdd[d.id] = name.data;
+        // console.log("-----:", d.id , " == ", name.data);
 
-  //     });
-  //     _.extendOwn(dependencies, depsToAdd);
-  //     root.refresh();
-  //   });
-  // });
+      });
+      _.extendOwn(dependencies, depsToAdd);
+      root.refresh();
+    });
+  });
 };
 
 var dataSource = function(callback) {
