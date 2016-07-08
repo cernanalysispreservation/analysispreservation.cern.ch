db8f29500e84b17b1b05c56ce8058494e097a0ef
Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>
2016-07-08 10:42:26 +0200

    tests: introducing test suite
    * Adds test for validating $ref urls.
    
    * #140.
    
    Signed-off-by: Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of CERN Analysis Preservation Framework. 
+# Copyright (C) 2016 CERN.
+#
+# CERN Analysis Preservation Framework is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# CERN Analysis Preservation Framework is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CERN Analysis Preservation Framework; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""CERN Analysis Preservation Framework overlay repository for Invenio."""
+
 import os
 import sys
 
@@ -8,22 +34,22 @@ from setuptools.command.test import test as TestCommand
 readme = ""
 # history = open('CHANGES.rst').read()
 history = ""
-# tests_require = [
-#     'check-manifest>=0.25',
-#     'coverage>=4.0',
-#     'isort>=4.2.2',
-#     'pydocstyle>=1.0.0',
-#     'pytest-cache>=1.0',
-#     'pytest-cov>=1.8.0',
-#     'pytest-pep8>=1.0.6',
-#     'pytest>=2.8.0',
-# ]
+tests_require = [
+    'check-manifest>=0.25',
+    'coverage>=4.0',
+    'isort>=4.2.2',
+    'pydocstyle>=1.0.0',
+    'pytest-cache>=1.0',
+    'pytest-cov>=1.8.0',
+    'pytest-pep8>=1.0.6',
+    'pytest>=2.8.0',
+]
 
 extras_require = {
     'docs': [
         'Sphinx>=1.3',
-    ]
-    # 'tests': tests_require,
+    ],
+    'tests': tests_require,
 }
 
 extras_require['all'] = []
@@ -65,35 +91,35 @@ install_requires = [
 packages = find_packages()
 
 
-# class PyTest(TestCommand):
-#     """PyTest Test."""
+class PyTest(TestCommand):
+    """PyTest Test."""
 
-#     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
-#     def initialize_options(self):
-#         """Init pytest."""
-#         TestCommand.initialize_options(self)
-#         self.pytest_args = []
-#         try:
-#             from ConfigParser import ConfigParser
-#         except ImportError:
-#             from configparser import ConfigParser
-#         config = ConfigParser()
-#         config.read('pytest.ini')
-#         self.pytest_args = config.get('pytest', 'addopts').split(' ')
+    def initialize_options(self):
+        """Init pytest."""
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+        try:
+            from ConfigParser import ConfigParser
+        except ImportError:
+            from configparser import ConfigParser
+        config = ConfigParser()
+        config.read('pytest.ini')
+        self.pytest_args = config.get('pytest', 'addopts').split(' ')
 
-#     def finalize_options(self):
-#         """Finalize pytest."""
-#         TestCommand.finalize_options(self)
-#         self.test_args = []
-#         self.test_suite = True
+    def finalize_options(self):
+        """Finalize pytest."""
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-#     def run_tests(self):
-#         """Run tests."""
-#         # import here, cause outside the eggs aren't loaded
-#         import pytest
-#         errno = pytest.main(self.pytest_args)
-#         sys.exit(errno)
+    def run_tests(self):
+        """Run tests."""
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 # Get the version string. Cannot be done with import!
 g = {}
@@ -194,7 +220,7 @@ setup(
     extras_require=extras_require,
     install_requires=install_requires,
     setup_requires=setup_requires,
-    # tests_require=tests_require,
+    tests_require=tests_require,
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
