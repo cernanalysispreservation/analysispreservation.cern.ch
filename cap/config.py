6e140c2ecbd5840757ba75626a9fcead1eaf7b0e
Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>
2016-07-14 16:54:40 +0200

    travis: adds script to recreate db
    * refactors config.py
    
    Signed-off-by: Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>

--- cap/config.py
+++ cap/config.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
+#
+# This file is part of CERN Analysis Preservation Framework.
+# Copyright (C) 2015, 2016 CERN.
+#
+# CERN Analysis Preservation Framework is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# CERN Analysis Preservation Framework is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CERN Analysis Preservation Framework; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
 
-"""cap base Invenio configuration."""
+
+"""CERN Analysis Preservation base configuration."""
 
 from __future__ import absolute_import, print_function
 import os
@@ -8,39 +31,50 @@ import copy
 from invenio_oauthclient.contrib import cern
 
 
-# Identity function for string extraction
 def _(x):
+    """Identity function for string extraction"""
     return x
 
 DEBUG = True
 
-# Default language and timezone
+# Mail
+# ====
+#: Regex for email
+EMAIL_REGEX = '[^@]+@[^@]+\.[^@]+'
+MAIL_SUPPRESS_SEND = True
+
+# Language
+# ========
+#: Default language
 BABEL_DEFAULT_LANGUAGE = 'en'
+#: Default timezone
 BABEL_DEFAULT_TIMEZONE = 'Europe/Zurich'
-I18N_LANGUAGES = [
-]
-
-BASE_TEMPLATE = "invenio_theme/page.html"
-COVER_TEMPLATE = "invenio_theme/page_cover.html"
-SETTINGS_TEMPLATE = 'cap_theme/settings/base.html'
-
-# WARNING: Do not share the secret key - especially do not commit it to
-# version control.
-SECRET_KEY = "changeme"
+#: Other supported languages.
+I18N_LANGUAGES = []
 
 # Theme
+# =====
+#: Default site name.
 THEME_SITENAME = _("CERN Analysis Preservation")
+#: Path to logo file.
 THEME_LOGO = "img/cap_logo_lrg.svg"
+#: Google Site Verification ids.
 THEME_GOOGLE_SITE_VERIFICATION = []
-
+#: Base template for entire site.
+BASE_TEMPLATE = "cap_theme/page.html"
+#: Cover template for entire site.
+COVER_TEMPLATE = "invenio_theme/page_cover.html"
+#: Settings template for entire site.
+SETTINGS_TEMPLATE = 'cap_theme/settings/base.html'
+#: JavaScript file containing the require.js build configuration.
 REQUIREJS_CONFIG = 'js/cap-build.js'
 
-RECORDS_UI_BASE_TEMPLATE = 'records/detail.html'
-RECORDS_UI_TOMBSTONE_TEMPLATE = 'records/detail.html'
-
-# Records configuration
+# Records
+# =======
+#: Records configuration
 RECORDS_UI_DEFAULT_PERMISSION_FACTORY = "cap.modules.theme.permissions:read_permission_factory"
 
+#: Endpoints for displaying records.
 RECORDS_UI_ENDPOINTS = dict(
     recid=dict(
         pid_type='recid',
@@ -49,8 +83,37 @@ RECORDS_UI_ENDPOINTS = dict(
     ),
 )
 
-EMAIL_REGEX = '[^@]+@[^@]+\.[^@]+'
+#: Records REST API endpoints.
+RECORDS_REST_ENDPOINTS = dict(
+    recid=dict(
+        pid_type='recid',
+        pid_minter='cap_record_minter',
+        pid_fetcher='cap_record_fetcher',
+        search_index='_all',
+        search_type=None,
+        record_serializers={
+            'application/json': ('cap.modules.records.serializers'
+                                 ':json_v1_response'),
+        },
+        search_serializers={
+            'application/json': ('cap.modules.records.serializers'
+                                 ':json_v1_search'),
+        },
+        list_route='/records/',
+        item_route='/records/<pid_value>',
+        default_media_type='application/json',
+    ),
+)
 
+#: Default base template.
+RECORDS_UI_BASE_TEMPLATE = 'records/detail.html'
+#: Default tombstone template.
+RECORDS_UI_TOMBSTONE_TEMPLATE = 'records/detail.html'
+
+
+# CAP collaboration groups
+# ========================
+#: Configuration for collaborations
 CAP_COLLAB_EGROUPS = dict(
     CMS=dict(
         collaboration_cms=[
@@ -74,50 +137,37 @@ CAP_COLLAB_EGROUPS = dict(
     )
 )
 
+# Search
+# ======
+#: Default API endpoint for search UI.
+SEARCH_UI_SEARCH_API = '/api/records/'
+
+#: Default ElasticSearch hosts
+SEARCH_ELASTIC_HOSTS = ["localhost:9200"]
+
+#: Search query enhancers
+SEARCH_QUERY_ENHANCERS = [
+    'cap.modules.access.ext:authenticated_query'
+]
+
+
+# Accounts
+# ========
+#: Login registration template.
+SECURITY_LOGIN_USER_TEMPLATE = "access/login_user.html"
+#: Login confirmation mail.
+SECURITY_SEND_REGISTER_EMAIL = False
+
 ACCOUNTS_REGISTER_BLUEPRINT = False
 SECURITY_POST_CHANGE_VIEW = False
 SECURITY_RECOVERABLE = False
 SECURITY_REGISTERABLE = False
 SECURITY_CHANGEABLE = False
-OAUTHCLIENT_LOGIN_USER_TEMPLATE = 'access/login_user.html'
+SECURITY_SEND_REGISTER_EMAIL = False
 
-RECORDS_REST_ENDPOINTS = dict(
-    recid=dict(
-        pid_type='recid',
-        pid_minter='cap_record_minter',
-        pid_fetcher='cap_record_fetcher',
-        search_index='_all',
-        search_type=None,
-        record_serializers={
-            'application/json': ('cap.modules.records.serializers'
-                                 ':json_v1_response'),
-        },
-        search_serializers={
-            'application/json': ('cap.modules.records.serializers'
-                                 ':json_v1_search'),
-        },
-        list_route='/records/',
-        item_route='/records/<pid_value>',
-        default_media_type='application/json',
-    ),
-)
-
-# SearchUI API endpoint.
-SEARCH_UI_SEARCH_API = '/api/records/'
-
-# Database
-# SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://localhost/cap"
-
-# ElasticSearch
-SEARCH_ELASTIC_HOSTS = ["localhost:9200"]
-
-# Path to where JSON metadata exist
-JSON_METADATA_PATH = "/_metadata"
-
-# Mail
-MAIL_SUPPRESS_SEND = True
-
-# OAuth configuration
+# Logging
+# =======
+#: CERN OAuth configuration
 CERN_APP_CREDENTIALS = {
     'consumer_key': os.environ.get('CERN_APP_CREDENTIALS_KEY'),
     'consumer_secret': os.environ.get('CERN_APP_CREDENTIALS_SECRET'),
@@ -127,13 +177,33 @@ CERN_REMOTE_APP = copy.deepcopy(cern.REMOTE_APP)
 CERN_REMOTE_APP["params"].update({
     'request_token_params': {
         "scope": "Email Groups",
-     }
+    }
 })
 OAUTHCLIENT_REMOTE_APPS = {'cern': CERN_REMOTE_APP}
-SECURITY_SEND_REGISTER_EMAIL = False
+#: OAuth login template.
+OAUTHCLIENT_LOGIN_USER_TEMPLATE = 'access/login_user.html'
 
-BASE_TEMPLATE = "cap_theme/page.html"
-SECURITY_LOGIN_USER_TEMPLATE = "access/login_user.html"
+# JSON Schemas
+# ============
+#: Hostname for JSON Schemas.
+JSONSCHEMAS_HOST = 'http://localhost:5000'
+#: Path to where JSON metadata exist
+JSON_METADATA_PATH = "/_metadata"
+
+# User profile
+# ============
+#: Enable all the users to perform all the actions
+ENABLE_SUPERPOWERS_FOR_EVERYONE = False
+
+# WARNING: Do not share the secret key - especially do not commit it to
+# version control.
+SECRET_KEY = "changeme"
+
+
+# Database
+# ============
+# SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://localhost/cap"
+
 # config.setdefault(
 #     'COVER_TEMPLATE', 'invenio_theme/page_cover.html')
 # config.setdefault(
@@ -154,12 +224,3 @@ SECURITY_LOGIN_USER_TEMPLATE = "access/login_user.html"
 #     'THEME_404_TEMPLATE', 'invenio_theme/404.html')
 # config.setdefault(
 #     'THEME_500_TEMPLATE', 'invenio_theme/500.html')
-
-SEARCH_QUERY_ENHANCERS = [
-    'cap.modules.access.ext:authenticated_query'
-]
-
-JSONSCHEMAS_HOST = 'https://localhost:5000'
-
-ENABLE_SUPERPOWERS_FOR_EVERYONE = False
-"""Enable all the users to perform all the actions."""
---
+++ scripts/init-db.sh
@@ -0,0 +1,17 @@
+# Destroy db
+cap db destroy --yes-i-know
+# Init and create db
+cap db init
+cap db create
+
+# Create collections
+cap collections create CERNAnalysisPreservation
+cap collections create CMS -p CERNAnalysisPreservation
+cap collections create CMSQuestionnaire -p CMS -q '_type:cmsquestionnaire'
+cap collections create CMSAnalysis -p CMS -q '_type:cmsanalysis'
+cap collections create LHCb -p CERNAnalysisPreservation
+cap collections create LHCbAnalysis -p LHCb -q '_type:lhcbanalysis'
+cap collections create ATLAS -p CERNAnalysisPreservation
+cap collections create ATLASWorkflows -p ATLAS -q '_type:atlasworkflows'
+cap collections create ATLASAnalysis -p ATLAS -q '_type:atlasanalysis'
+cap collections create ALICE -p CERNAnalysisPreservation
