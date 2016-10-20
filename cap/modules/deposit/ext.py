68f9dc5de844ff9587c1205540d172ef9f6487da
Anna Trzcinska <trzcinska.ania@gmail.com>
2016-10-20 14:03:52 +0200

    deposit: index record in ES after being published
    * Closes #302
    
    Signed-off-by: Anna Trzcinska <trzcinska.ania@gmail.com>

--- cap/modules/deposit/ext.py
+++ cap/modules/deposit/ext.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import, print_function
 
-from invenio_indexer.signals import before_record_index
-from invenio_deposit.config import DEPOSIT_UI_NEW_TEMPLATE, \
-    DEPOSIT_UI_INDEX_TEMPLATE
 from cap.config import DEPOSIT_GROUPS
+from invenio_deposit.config import (DEPOSIT_UI_INDEX_TEMPLATE,
+                                    DEPOSIT_UI_NEW_TEMPLATE)
+from invenio_deposit.signals import post_action
 
+from .indexers import index_published_record
 from .views.ui import create_blueprint as ui_blueprint
 
 
@@ -15,12 +16,15 @@ class CAPDeposit(object):
         """Extension initialization."""
         if app:
             self.init_app(app)
+        self.register_signals()
 
     def init_app(self, app):
         """Flask application initialization."""
         app.register_blueprint(ui_blueprint())
         app.extensions['cap_deposit'] = self
 
+    def register_signals(self):
+        post_action.connect(index_published_record)
 
     # @staticmethod
     # def init_config(app):
