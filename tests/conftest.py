186245d8b5d944bd3cbd41081a1bca7dfedf6777
Anna Trzcinska <anna.trzcinska@cern.ch>
2018-01-17 16:12:16 +0100

    tests: create integration tests setup
    Signed-off-by: Anna Trzcinska <anna.trzcinska@cern.ch>

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,23 +30,28 @@ from __future__ import absolute_import, print_function
 import os
 import shutil
 import tempfile
-
+from copy import deepcopy
+from datetime import datetime, timedelta
 from uuid import uuid4
 
-
 import pytest
+from elasticsearch.exceptions import RequestError
+from flask_security import login_user
+from invenio_access.models import ActionUsers
+from invenio_accounts.testutils import create_test_user
+from invenio_admin.permissions import action_admin_access
+from invenio_db import db as db_
+from invenio_deposit.minters import deposit_minter
+from invenio_deposit.permissions import \
+    action_admin_access as deposit_admin_access
+from invenio_deposit.scopes import write_scope
+from invenio_files_rest.models import Location
+from invenio_oauth2server.models import Client, Token
+from invenio_search import current_search, current_search_client
+from sqlalchemy_utils.functions import create_database, database_exists
 
 from cap.factory import create_api
 from cap.modules.deposit.api import CAPDeposit as Deposit
-from elasticsearch.exceptions import RequestError
-from flask_cli import ScriptInfo
-from flask_security import login_user
-from invenio_accounts.testutils import create_test_user
-from invenio_db import db as db_
-from invenio_deposit.minters import deposit_minter
-from invenio_files_rest.models import Location
-from invenio_search import current_search, current_search_client
-from sqlalchemy_utils.functions import create_database, database_exists
 
 
 @pytest.yield_fixture(scope='session')
@@ -90,13 +95,7 @@ def app(env_config, default_config):
         yield app
 
 
-@pytest.yield_fixture(scope='session')
-def script_info(app):
-    """Ensure that the database schema is created."""
-    yield ScriptInfo(create_app=lambda info: app)
-
-
-@pytest.yield_fixture(scope='session')
+@pytest.yield_fixture()
 def db(app):
     """Setup database."""
     if not database_exists(str(db_.engine.url)):
@@ -125,10 +124,89 @@ def users(app, db):
     """Create users."""
     user1 = create_test_user(
         email='cms@inveniosoftware.org', password='cmscms')
+    superuser = create_test_user(
+        email='admin@inveniosoftware.org', password='adminadmin')
+
+    with db.session.begin_nested():
+        # set admin permissions
+        db.session.add(ActionUsers(action=action_admin_access.value,
+                                   user=superuser))
+        db.session.add(ActionUsers(action=deposit_admin_access.value,
+                                   user=superuser))
+
+    db.session.commit()
 
     return [
-        {'email': user1.email, 'id': user1.id}
+        {'email': superuser.email, 'id': superuser.id}
     ]
+
+
+@pytest.fixture
+def oauth2_client(app, db, users):
+    """Create client."""
+    with db.session.begin_nested():
+        # create resource_owner -> client_1
+        client_ = Client(
+            client_id='client_test_u1c1',
+            client_secret='client_test_u1c1',
+            name='client_test_u1c1',
+            description='',
+            is_confidential=False,
+            user_id=users[0]['id'],
+            _redirect_uris='',
+            _default_scopes='',
+        )
+        db.session.add(client_)
+
+    db.session.commit()
+    return client_.client_id
+
+
+@pytest.fixture
+def write_token(app, db, oauth2_client, users):
+    """Create token."""
+    with db.session.begin_nested():
+        token_ = Token(
+            client_id=oauth2_client,
+            user_id=users[0]['id'],
+            access_token='dev_access_2',
+            refresh_token='dev_refresh_2',
+            expires=datetime.utcnow() + timedelta(hours=10),
+            is_personal=False,
+            is_internal=True,
+            _scopes=write_scope.id,
+        )
+        db.session.add(token_)
+    db.session.commit()
+    return dict(
+        token=token_,
+        auth_header=[
+            ('Authorization', 'Bearer {0}'.format(token_.access_token)),
+        ]
+    )
+
+
+@pytest.fixture
+def json_headers():
+    """JSON headers."""
+    return [('Content-Type', 'application/json'),
+            ('Accept', 'application/json')]
+
+
+@pytest.fixture
+def json_auth_headers(json_headers, write_token):
+    """Authentication headers (with a valid oauth2 token).
+    It uses the token associated with the first user.
+    """
+    return bearer_auth(json_headers, write_token)
+
+
+@pytest.fixture
+def auth_headers(write_token):
+    """Authentication headers (with a valid oauth2 token).
+    It uses the token associated with the first user.
+    """
+    return bearer_auth([], write_token)
 
 
 @pytest.yield_fixture()
@@ -150,6 +228,21 @@ def location(db):
 
 
 @pytest.fixture()
+def deposit(app, es, users, location, deposit_metadata):
+    """New deposit with files."""
+    with app.test_request_context():
+        datastore = app.extensions['security'].datastore
+        login_user(datastore.get_user(users[0]['email']))
+        id_ = uuid4()
+        deposit_minter(id_, deposit_metadata)
+        deposit = Deposit.create(deposit_metadata, id_=id_)
+        db_.session.commit()
+    current_search.flush_and_refresh(
+        index='deposits-records-cms-analysis-v0.0.1')
+    return deposit
+
+
+@pytest.fixture()
 def deposit_metadata():
     """Raw metadata of deposit."""
     data = {
@@ -164,16 +257,10 @@ def deposit_metadata():
     return data
 
 
-@pytest.fixture()
-def deposit(app, es, users, location, deposit_metadata):
-    """New deposit with files."""
-    with app.test_request_context():
-        datastore = app.extensions['security'].datastore
-        login_user(datastore.get_user(users[0]['email']))
-        id_ = uuid4()
-        deposit_minter(id_, deposit_metadata)
-        deposit = Deposit.create(deposit_metadata, id_=id_)
-        db_.session.commit()
-    current_search.flush_and_refresh(
-        index='deposits-records-cms-analysis-v0.0.1')
-    return deposit
+def bearer_auth(headers, token):
+    """Create authentication headers (with a valid oauth2 token)."""
+    headers = deepcopy(headers)
+    headers.append(
+        ('Authorization', 'Bearer {0}'.format(token['token'].access_token))
+    )
+    return headers
--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of CERN Analysis Preservation Framework.
-# Copyright (C) 2016 CERN.
+# Copyright (C) 2017 CERN.
 #
 # CERN Analysis Preservation Framework is free software; you can redistribute
 # it and/or modify it under the terms of the GNU General Public License as
@@ -21,38 +21,24 @@
 # In applying this license, CERN does not
 # waive the privileges and immunities granted to it by virtue of its status
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
+# or submit itself to any jurisdiction.
 
-"""CERN Analysis Preservation fixtures CLI test cases."""
+"""Unit tests Cap Deposit api."""
 
 from __future__ import absolute_import, print_function
 
-# from click.testing import CliRunner
-# from invenio_pages.models import Page
 
-# from cap.modules.fixtures.cli import loadpages_cli
+def test_example(app, db, users, write_token, es):
+    """ Example test to show how to set up tests for API calls."""
+    auth = write_token['auth_header']
+    headers = [
+        ('Content-Type', 'application/json'),
+        ('Accept', 'application/json')
+    ]
+    auth_headers = headers + auth
 
+    with app.test_client() as client:
+        resp = client.get('/ping', headers=auth_headers)
 
-def test_loadpages(script_info, db):
-    """Test version import."""
-    #
-    # TO FIX when pages-REST are added
-    pass
-    # assert Page.query.count() == 0
-    # runner = CliRunner()
-    # res = runner.invoke(loadpages_cli, [], obj=script_info)
-    # assert res.exit_code == 0
-    # assert Page.query.count() == 2
-    # page = Page.query.filter_by(url='/about').one()
-    # assert page.title == 'About Cap'
-    # assert len(page.description) > 20
-    # assert len(page.content) > 100
-    # assert page.template_name == 'invenio_pages/dynamic.html'
-    # res = runner.invoke(loadpages_cli, [], obj=script_info)
-    # assert res.exit_code != 0
-    # res = runner.invoke(loadpages_cli, ['-f'], obj=script_info)
-    # assert res.exit_code == 0
-
-    # for p in Page.query.all():
-    #     assert p.title
-    #     assert p.url
-    #     assert p.template_name
+        assert resp.status_code == 200
+        assert "Pong" in resp.data
