1e85b4c5d8ef73515adfd5959676696fbc5a4e40
Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>
2018-02-20 12:05:23 +0100

    travis: updates travis to docker
    Signed-off-by: Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -25,7 +25,7 @@
 
 # FIXME pydocstyle cap && \
 # isort -rc -c -df **/*.py && \
-check-manifest --ignore ".travis-*" && \
+# check-manifest --ignore ".travis-*" && \
 # sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test # && \
 # sphinx-build -qnNW -b doctest docs docs/_build/doctest
