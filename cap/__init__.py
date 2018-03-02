1aa4c90924262aa9fd6cfaee7a80d09e0f36e7bb
Pamfilos Fokianos <pamfilosf@gmail.com>
2018-03-02 15:35:25 +0100

    system: removes 'ServerConf' warning
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- cap/__init__.py
+++ cap/__init__.py
@@ -3,16 +3,3 @@
 """cap."""
 
 from __future__ import absolute_import, print_function
-
-from .config import GITLAB_OAUTH_ACCESS_TOKEN
-
-
-class ServerConfigurationException(Exception):
-    """Custom exception for gitlab oauth."""
-
-    def __init__(self):
-        super(ServerConfigurationException, self).__init__('No gitlab access token provided')
-
-
-if GITLAB_OAUTH_ACCESS_TOKEN is None:
-    raise ServerConfigurationException()
