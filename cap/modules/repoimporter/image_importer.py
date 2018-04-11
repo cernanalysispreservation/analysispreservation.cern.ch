871eeb5654de390243262fe6ff9a09917703d425
Anna Trzcinska <anna.trzcinska@cern.ch>
2018-04-11 13:16:47 +0200

    importer: fix image importer
    Signed-off-by: Anna Trzcinska <anna.trzcinska@cern.ch>

--- cap/modules/repoimporter/image_importer.py
+++ cap/modules/repoimporter/image_importer.py
@@ -1,78 +1,28 @@
-import docker
-import requests
-from docker.errors import DockerException
-from subprocess import check_call, CalledProcessError
+from subprocess import check_call
 
 
 class ImageImporter:
     """ ImageImporter saves docker images
 
     Attributes:
-        image_name  eg. gitlab-registry.cern.ch/atrisovi/test-dockint
-        image_tag   optional
-        location    location to save the image at
         username    username
         token       private token
     """
 
     def __init__(self, username=None, token=None):
-        if username is None or token is None:
-            self.auth_config = None
-        else:
-            self.auth_config = {
-                "username": username,
-                "password": token
-            }
         self.username = username
         self.token = token
-        self.image_name = None
-        self.image_tag = None
-        self.location = None
 
-    def archive_image(self, image_name, location, image_tag=None):
+    def archive_image(self, image_name, location):
         """ Archives image """
-        if ':' in image_name:
-            self.image_name, self.image_tag = image_name.split(':')
-        else:
-            self.image_name = image_name
-            self.image_tag = "latest"
-        if image_tag is not None:
-            self.image_tag = image_tag
+        location = location.rstrip('/')
 
-        self.location = location
-        if self.location[-1] == '/':
-            self.location = location[:-1]
-
-        try:
-            self.archive_image_with_python()
-        except (DockerException, requests.exceptions.Timeout) as e:
-            try:  # try another method
-                self.archive_image_with_skopeo()
-            except CalledProcessError:
-                return
-
-    def archive_image_with_python(self):
-        """ Archives image with docker-py. """
-        self.location = '{}/{}_{}'.format(self.location, self.image_name.replace('/', '_'),
-                                          self.image_tag)
-        client = docker.from_env()
-        img_name = '{}:{}'.format(self.image_name, self.image_tag)
-        if self.auth_config:
-            img = client.images.pull(img_name, auth_config=self.auth_config)
-        else:
-            img = client.images.pull(img_name)
-        with open('{}.tar'.format(self.location), 'w') as f:
-            resp = img.save()
-            for chunk in resp:
-                f.write(chunk)
-        client.images.remove(img_name)
-
-    def archive_image_with_skopeo(self):
-        """ Archives image with skopeo. """
         if self.token is None:
-            check_call(["skopeo", "copy", "docker://{}".format(self.image_name),
-                        "dir:{}".format(self.location)])
+            check_call(["skopeo", "copy",
+                        "docker://{}".format(image_name),
+                        "dir:{}".format(location)])
         else:
             cred = "--src-creds={}:{}".format(self.username, self.token)
-            check_call(["skopeo", "copy", cred, "docker://{}".format(self.image_name),
-                        "dir:{}".format(self.location)])
+            check_call(["skopeo", "copy", cred,
+                        "docker://{}".format(image_name),
+                        "dir:{}".format(location)])
--- tests/unit/repoimporter/test_archive_image.py
+++ tests/unit/repoimporter/test_archive_image.py
@@ -24,7 +24,7 @@
 # or submit itself to any jurisdiction.
 
 from __future__ import absolute_import, print_function
-import mock
+
 from cap.modules.repoimporter.image_importer import ImageImporter
 
 
@@ -32,52 +32,3 @@ def test_image_importer_if_username_and_token_in_params():
     image_importer = ImageImporter(username='atrisovi', token='fun_token')
     assert image_importer.username == 'atrisovi'
     assert image_importer.token == 'fun_token'
-    assert image_importer.image_name is None
-    assert image_importer.image_tag is None
-    assert image_importer.location is None
-
-
-def test_image_importer_if_no_username_and_token_in_params():
-    image_importer = ImageImporter()
-    assert image_importer.username is None
-    assert image_importer.token is None
-    assert image_importer.image_name is None
-    assert image_importer.image_tag is None
-    assert image_importer.location is None
-
-
-def test_archive_image_if_colon_in_image_name():
-    image_importer = ImageImporter()
-    image_importer.archive_image_with_python = mock.Mock()
-    image_importer.archive_image(
-        "gitlab-registry.cern.ch/atrisovi/test-dockint:hello", "tmp")
-    assert image_importer.image_name == \
-        'gitlab-registry.cern.ch/atrisovi/test-dockint'
-    assert image_importer.image_tag == 'hello'
-
-
-def test_archive_image_if_no_colon_in_image_name():
-    image_importer = ImageImporter()
-    image_importer.archive_image_with_python = mock.Mock()
-    image_importer.archive_image(
-        "gitlab-registry.cern.ch/atrisovi/test-dockint", "tmp")
-    assert image_importer.image_name == \
-        'gitlab-registry.cern.ch/atrisovi/test-dockint'
-    assert image_importer.image_tag == 'latest'
-
-
-def test_archive_image_if_image_tag_in_params():
-    image_importer = ImageImporter()
-    image_importer.archive_image_with_python = mock.Mock()
-    image_importer.archive_image(
-        "gitlab-registry.cern.ch/atrisovi/test-dockint", "tmp",
-        image_tag='my_image')
-    assert image_importer.image_tag == 'my_image'
-
-
-def test_archive_image_if_location_with_slash():
-    image_importer = ImageImporter()
-    image_importer.archive_image_with_python = mock.Mock()
-    image_importer.archive_image(
-        "gitlab-registry.cern.ch/atrisovi/test-dockint:hello", "tmp/")
-    assert image_importer.location == 'tmp'
