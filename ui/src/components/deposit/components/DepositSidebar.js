6a2db7ee8cc6e81a194a7bfea21d1154f8ebeaba
Pamfilos Fokianos <pamfilosf@gmail.com>
2018-06-18 13:36:38 +0200

    ui: removes type from DepositSidebar
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- ui/src/components/deposit/components/DepositSidebar.js
+++ ui/src/components/deposit/components/DepositSidebar.js
@@ -56,7 +56,6 @@ class DepositSidebar extends React.Component {
           <Box pad="medium">
             <Title>Start Your Project</Title>
             <Paragraph>Give a name to your project..This way you can recognise it between your drafts</Paragraph>
-            <Paragraph>{this.props.match.params.schema_id}</Paragraph>
             <Form schema={{type: "string", title: "Project Name"}} onSubmit={this._onSubmit.bind(this, this.props.match.params.schema_id)} >
               <Box flex={true} margin={{vertical: "medium"}}>
                 <Button label='Start Project'
--- ui/src/store/configureStore.js
+++ ui/src/store/configureStore.js
@@ -10,7 +10,6 @@ import { routerMiddleware } from 'react-router-redux';
 import {createLogger} from 'redux-logger';
 
 import rootReducer from '../reducers';
-import promiseMiddleware from 'redux-promise';
 
 export const history = createHistory();
 
@@ -47,7 +46,6 @@ function configureStoreDev(initialState) {
 
         // thunk middleware can also accept an extra argument to be passed to each thunk action
         // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-        promiseMiddleware,
         reactRouterMiddleware,
         thunk,
         loggerMiddleware,
