924c64efc7a29c1e3554cb46cc911ff5f97b346d
Pamfilos Fokianos <pamfilosf@gmail.com>
2018-09-26 16:58:42 +0200

    tests: ignore UI dirs silent PEP8 in tests
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- scripts/schemas.py
+++ scripts/schemas.py
@@ -25,7 +25,8 @@
 import errno
 import os
 
-PROJECT_BASE = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+PROJECT_BASE = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), os.pardir)
 JSONSCHEMAS_DIR = os.path.join(
     PROJECT_BASE,
     'cap',
