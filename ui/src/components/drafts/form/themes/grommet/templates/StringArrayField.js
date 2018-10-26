c381084ae185d3074978e7cc0394efc10add2fd5
Anna Trzcinska <anna.trzcinska@cern.ch>
2018-10-26 11:20:19 +0200

    ui: add trash icon to remove fields
    * closes #975
    
    Signed-off-by: Anna Trzcinska <anna.trzcinska@cern.ch>

--- ui/src/components/drafts/form/themes/grommet/templates/StringArrayField.js
+++ ui/src/components/drafts/form/themes/grommet/templates/StringArrayField.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Box, List, ListItem } from "grommet";
+import { Button, Box, List, ListItem } from "grommet";
 
 import TextWidget from "../widgets/TextWidget";
+import FormTrashIcon from "grommet/components/icons/base/FormTrash";
 
 class StringArrayField extends React.Component {
   constructor(props) {
@@ -19,7 +20,7 @@ class StringArrayField extends React.Component {
               {this.props.items.length > 0
                 ? this.props.items.map(element => (
                     <ListItem key={element.index} separator="none" pad="none">
-                      <Box flex={true}>
+                      <Box flex={true} direction="row">
                         <TextWidget
                           {...element.children.props}
                           value={element.children.props.formData}
@@ -35,6 +36,12 @@ class StringArrayField extends React.Component {
                               this.props.onAddClick(event);
                             }
                           }}
+                        />
+                        <Button
+                          onClick={event =>
+                            element.onDropIndexClick(element.index)(event)
+                          }
+                          icon={<FormTrashIcon />}
                         />
                       </Box>
                     </ListItem>
