e6142285e7c241314e1df4e44daaba09eb44ef94
Pamfilos Fokianos <pamfilosf@gmail.com>
2018-11-01 13:40:24 +0100

    ui: update build webpack config
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- ui/webpack.config.prod.js
+++ ui/webpack.config.prod.js
@@ -1,42 +1,43 @@
 // For info about this file refer to webpack and webpack-hot-middleware documentation
 // For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
-import webpack from 'webpack';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import WebpackMd5Hash from 'webpack-md5-hash';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
 
 const GLOBALS = {
-  'process.env.NODE_ENV': JSON.stringify('production'),
+  "process.env.NODE_ENV": JSON.stringify("production"),
   __DEV__: false
 };
 
 export default {
   resolve: {
-    extensions: ['*', '.js', '.jsx', '.json']
+    extensions: ["*", ".js", ".jsx", ".json"]
   },
-  devtool: 'source-map', // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
-  entry: path.resolve(__dirname, 'src/index'),
-  target: 'web',
+  devtool: "source-map", // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
+  entry: path.resolve(__dirname, "src/index"),
+  target: "web",
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].[chunkhash].js'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].[contenthash].js"
   },
   plugins: [
-    // Hash the files using MD5 so that their names change when the content changes.
-    new WebpackMd5Hash(),
-
     // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
     new webpack.DefinePlugin(GLOBALS),
 
     // Generate an external css file with a hash in the filename
-    new ExtractTextPlugin('[name].[contenthash].css'),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    }),
+    // Generate an external css file with a hash in the filename
+    // new ExtractTextPlugin('[name].[contenthash].css'),
 
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
-      template: 'src/index.ejs',
-      favicon: 'src/favicon.ico',
+      template: "src/index.ejs",
+      favicon: "src/favicon.ico",
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -52,26 +53,26 @@ export default {
       inject: true,
       // Note that you can add custom options here if you need to handle other custom logic in index.html
       // To track JavaScript errors via TrackJS, sign up for a free trial at TrackJS.com and enter your token below.
-      trackJSToken: ''
-    }),
+      trackJSToken: ""
+    })
 
     // Minify JS
-    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+    // new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
   ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ["babel-loader"]
       },
       {
         test: /\.eot(\?v=\d+.\d+.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              name: '[name].[ext]'
+              name: "[name].[ext]"
             }
           }
         ]
@@ -80,11 +81,11 @@ export default {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000,
-              mimetype: 'application/font-woff',
-              name: '[name].[ext]'
+              mimetype: "application/font-woff",
+              name: "[name].[ext]"
             }
           }
         ]
@@ -93,11 +94,11 @@ export default {
         test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000,
-              mimetype: 'application/octet-stream',
-              name: '[name].[ext]'
+              mimetype: "application/octet-stream",
+              name: "[name].[ext]"
             }
           }
         ]
@@ -106,11 +107,11 @@ export default {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000,
-              mimetype: 'image/svg+xml',
-              name: '[name].[ext]'
+              mimetype: "image/svg+xml",
+              name: "[name].[ext]"
             }
           }
         ]
@@ -119,43 +120,41 @@ export default {
         test: /\.(jpe?g|png|gif|ico)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]'
+              name: "[name].[ext]"
             }
           }
         ]
       },
       {
         test: /(\.css|\.scss|\.sass)$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                minimize: true,
-                sourceMap: true
-              }
-            }, {
-              loader: 'postcss-loader',
-              options: {
-                plugins: () => [
-                  require('autoprefixer')
-                ],
-                sourceMap: true
-              }
-            }, {
-              loader: 'sass-loader',
-              options: {
-                includePaths: [
-                  './node_modules',
-                  path.resolve(__dirname, 'src', 'scss')
-                ],
-                sourceMap: true
-              }
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
             }
-          ]
-        })
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [require("cssnano"), require("autoprefixer")],
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              includePaths: [
+                "./node_modules",
+                path.resolve(__dirname, "src", "scss")
+              ],
+              sourceMap: true
+            }
+          }
+        ]
       }
     ]
   }
