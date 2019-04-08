c3bf8e6f59329b283374c338844379fb818a0671
Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>
2019-04-08 16:31:00 +0200

    ui: download file on publish
     * Closes #1121.
    
    Signed-off-by: Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>

--- ui/src/components/drafts/components/FileList.js
+++ ui/src/components/drafts/components/FileList.js
@@ -7,11 +7,11 @@ import ListPlaceholder from "grommet-addons/components/ListPlaceholder";
 
 import FileItem from "./FileItem";
 
-import ArchiveIcon from "grommet/components/icons/base/Archive";
-import DocumentConfigIcon from "grommet/components/icons/base/DocumentConfig";
-import PieChartIcon from "grommet/components/icons/base/PieChart";
-import BookIcon from "grommet/components/icons/base/Book";
-import NoteIcon from "grommet/components/icons/base/Note";
+// import ArchiveIcon from "grommet/components/icons/base/Archive";
+// import DocumentConfigIcon from "grommet/components/icons/base/DocumentConfig";
+// import PieChartIcon from "grommet/components/icons/base/PieChart";
+// import BookIcon from "grommet/components/icons/base/Book";
+// import NoteIcon from "grommet/components/icons/base/Note";
 
 import { connect } from "react-redux";
 
@@ -20,22 +20,22 @@ class FileList extends React.Component {
     super(props);
   }
 
-  _getIcon(type) {
-    const catToIcon = {
-      default: <ArchiveIcon type="status" size="xsmall" />,
-      archive: <ArchiveIcon type="status" size="xsmall" />,
-      configuration: <DocumentConfigIcon type="status" size="xsmall" />,
-      dataset: <PieChartIcon type="status" size="xsmall" />,
-      publication: <BookIcon type="status" size="xsmall" />,
-      plot: <PieChartIcon type="status" size="xsmall" />
-    };
+  // _getIcon(type) {
+  //   const catToIcon = {
+  //     default: <ArchiveIcon type="status" size="xsmall" />,
+  //     archive: <ArchiveIcon type="status" size="xsmall" />,
+  //     configuration: <DocumentConfigIcon type="status" size="xsmall" />,
+  //     dataset: <PieChartIcon type="status" size="xsmall" />,
+  //     publication: <BookIcon type="status" size="xsmall" />,
+  //     plot: <PieChartIcon type="status" size="xsmall" />
+  //   };
 
-    return catToIcon[type] ? (
-      catToIcon[type]
-    ) : (
-      <NoteIcon type="status" size="xsmall" />
-    );
-  }
+  //   return catToIcon[type] ? (
+  //     catToIcon[type]
+  //   ) : (
+  //       <NoteIcon type="status" size="xsmall" />
+  //     );
+  // }
 
   _actionItem(selected) {
     this.props.action(selected);
@@ -44,22 +44,39 @@ class FileList extends React.Component {
   render() {
     return (
       <List selectable={this.props.selectableActionLayer ? true : false}>
-        {this.props.files && this.props.files.size > 0 ? (
-          this.props.files
-            .keySeq()
-            .toArray()
-            .map((filename, index) => {
-              let file = this.props.files.get(filename);
+        {this.props.files ? (
+          Array.isArray(this.props.files) ? (
+            this.props.files.map((file, index) => {
               return (
                 <FileItem
-                  key={`${filename}-${index}`}
+                  key={`${file.key}-${index}`}
                   action={key =>
                     this.props.action ? this._actionItem.bind(this, key) : null
                   }
                   file={file}
+                  status={this.props.status}
                 />
               );
             })
+          ) : this.props.files.size > 0 ? (
+            this.props.files
+              .keySeq()
+              .toArray()
+              .map((filename, index) => {
+                let file = this.props.files.get(filename);
+                return (
+                  <FileItem
+                    key={`${filename}-${index}`}
+                    action={key =>
+                      this.props.action
+                        ? this._actionItem.bind(this, key)
+                        : null
+                    }
+                    file={file}
+                  />
+                );
+              })
+          ) : null
         ) : (
           <ListPlaceholder
             emptyMessage={
@@ -83,17 +100,10 @@ FileList.propTypes = {
   files: PropTypes.object
 };
 
-function mapStateToProps(state) {
+const mapStateToProps = state => {
   return {
     selectableActionLayer: state.drafts.get("fileManagerLayerSelectableAction")
   };
-}
+};
 
-function mapDispatchToProps() {
-  return {};
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(FileList);
+export default connect(mapStateToProps)(FileList);
