9eb130c9b2a8fd76cbd5d8e22477473c015ecf88
Pamfilos Fokianos <pamfilosf@gmail.com>
2019-07-15 13:27:51 +0200

    global: bump invenio-logging version
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ install_requires = [
     'Authlib==0.12.1',
     # "raven" versions needed till we FIX dependecies on installation
     'raven[flask]>=5.0.0,<5.5',
-    'invenio-logging[sentry]>=1.0.0b1',
+    'invenio-logging[sentry]>=1.0.0',
     'uWSGI==2.0.17',
     'uwsgi-tools==1.1.1',
     'uwsgitop==0.10',
