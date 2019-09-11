5fa91aa714a008412c069a94afae0ac3df34d6a4
Pamfilos Fokianos <pamfilosf@gmail.com>
2019-09-11 11:46:54 +0200

    ui: udpates base 'react', 'redux' dependencies
    * bumps 'react', 'react-dom', 'history', 'react-redux', 'react-router',
    'react-router-dom', 'react-router-redux', 'redux', 'redux-immutable',
    'redux-thunk'
    
    * 'react-router-redux' to better 'connected-react-router'

--- ui/src/components/Root.js
+++ ui/src/components/Root.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { ConnectedRouter } from 'react-router-redux';
-import { Provider } from 'react-redux';
-import App from './App';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { ConnectedRouter } from "react-router-redux";
+import { Provider } from "react-redux";
+import App from "./App";
 
 export default class Root extends Component {
   render() {
     const { store, history } = this.props;
     return (
       <Provider store={store}>
-        <ConnectedRouter history={history}>
+        <ConnectedRouter store={store} history={history}>
           <App />
         </ConnectedRouter>
       </Provider>
--- ui/src/store/configureStore.js
+++ ui/src/store/configureStore.js
@@ -1,74 +1,77 @@
-import {createStore, compose, applyMiddleware} from 'redux';
+import { createStore, compose, applyMiddleware } from "redux";
 // import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-import createHistory from 'history/createBrowserHistory';
-// import { browserHistory } from 'history';
+import thunk from "redux-thunk";
 
-// 'routerMiddleware': the new way of storing route changes with redux middleware since rrV4.
-import { routerMiddleware } from 'react-router-redux';
+import { createBrowserHistory } from "history";
+import { routerMiddleware } from "connected-react-router";
+import { createLogger } from "redux-logger";
 
-import {createLogger} from 'redux-logger';
+import rootReducer from "../reducers";
 
-import rootReducer from '../reducers';
-
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 function configureStoreProd(initialState) {
-    const reactRouterMiddleware = routerMiddleware(history);
-    const middlewares = [
-        // Add other middleware on this line...
+  const reactRouterMiddleware = routerMiddleware(history);
+  const middlewares = [
+    // Add other middleware on this line...
 
-        // thunk middleware can also accept an extra argument to be passed to each thunk action
-        // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-        reactRouterMiddleware,
-        thunk,
-    ];
+    // thunk middleware can also accept an extra argument to be passed to each thunk action
+    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+    reactRouterMiddleware,
+    thunk
+  ];
 
-    const store = createStore(rootReducer, initialState, compose(
-        applyMiddleware(...middlewares)
-    )
-    );
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middlewares))
+  );
 
-    return store;
+  return store;
 }
 
 function configureStoreDev(initialState) {
-    const reactRouterMiddleware = routerMiddleware(history);
-    const loggerMiddleware = createLogger();
+  const reactRouterMiddleware = routerMiddleware(history);
+  const loggerMiddleware = createLogger();
 
-    const middlewares = [
-        // Add other middleware on this line...
+  const middlewares = [
+    // Add other middleware on this line...
 
-        // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
-        // **REMARK_BY_PAM not needed if using ImmutableJS
-        // TOFIX remove libbrary dependency
-        // reduxImmutableStateInvariant(),
+    // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
+    // **REMARK_BY_PAM not needed if using ImmutableJS
+    // TOFIX remove libbrary dependency
+    // reduxImmutableStateInvariant(),
 
-        // thunk middleware can also accept an extra argument to be passed to each thunk action
-        // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-        reactRouterMiddleware,
-        thunk,
-        loggerMiddleware,
-    ];
+    // thunk middleware can also accept an extra argument to be passed to each thunk action
+    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+    reactRouterMiddleware,
+    thunk,
+    loggerMiddleware
+  ];
 
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-    const store = createStore(rootReducer, initialState, composeEnhancers(
-        applyMiddleware(...middlewares)
-    )
-    );
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+  const store = createStore(
+    rootReducer(history),
+    initialState,
+    composeEnhancers(applyMiddleware(...middlewares))
+  );
 
-    if (module.hot) {
-        // Enable Webpack hot module replacement for reducers
-        module.hot.accept('../reducers', () => {
-            const nextReducer = require('../reducers').default; // eslint-disable-line global-require
-            store.replaceReducer(nextReducer);
-        });
-    }
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept("../reducers", () => {
+      const nextReducer = require("../reducers").default; // eslint-disable-line global-require
+      store.replaceReducer(nextReducer);
+    });
+  }
 
-    return store;
+  return store;
 }
 
-const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
+const configureStore =
+  process.env.NODE_ENV === "production"
+    ? configureStoreProd
+    : configureStoreDev;
 const store = configureStore();
 
 export default store;
