0de988d0e3696ae3fff11f3c9684827f00c7df80
Pamfilos Fokianos <pamfilosf@gmail.com>
2019-09-24 21:08:48 +0200

    search: register mappings from DB schemas
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- cap/modules/deposit/ext.py
+++ cap/modules/deposit/ext.py
@@ -1,6 +1,8 @@
 """Initialize extension."""
 
 from __future__ import absolute_import, print_function
+from cap.modules.schemas.models import Schema
+from invenio_search import current_search
 
 
 class CAPDeposit(object):
@@ -14,3 +16,12 @@ class CAPDeposit(object):
     def init_app(self, app):
         """Flask application initialization."""
         app.extensions['cap_deposit'] = self
+
+        # fix to register mappings from DB
+        # [TOBE_FIXED] better query, etc
+        @app.before_first_request
+        def register_mappings():
+            schemas = Schema.query.all()
+            for schema in schemas:
+                current_search.mappings[schema.deposit_index] = {}
+                current_search.mappings[schema.record_index] = {}
