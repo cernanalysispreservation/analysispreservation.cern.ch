db06bcbf6d64fdf02ed61aacb5976e1bf78af9e4
Ilias Koutsakis <ilias.koutsakis@cern.ch>
2020-01-16 10:12:52 +0100

    services: add latex endpoint for paths
    Signed-off-by: Ilias Koutsakis <ilias.koutsakis@cern.ch>

---
+++ cap/modules/services/views/latex.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of CERN Analysis Preservation Framework.
+# Copyright (C) 2018 CERN.
+#
+# CERN Analysis Preservation Framework is free software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# CERN Analysis Preservation Framework is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CERN Analysis Preservation Framework; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+"""CAP Latex service."""
+
+from flask import jsonify, request, abort
+from . import blueprint
+from cap.modules.access.utils import login_required
+from cap.modules.services.serializers.latex import PathValidator
+
+TEMPLATE = """\\begin{{array}}{{ |c| }} \hline
+\\textbf{{{title}}} \\\ \hline
+{joined_paths}
+\end{{array}}
+"""
+
+NEW_LINE = ' \\\ \hline\n'
+
+
+def _latexify(paths, title):
+    data, errors = PathValidator().load({'paths': paths})
+    if errors != {}:
+        abort(400, 'Dataset list contains invalid values. Aborting.')
+
+    paths_fixed = [path.replace('_', '\_') for path in paths]
+    joined = NEW_LINE.join(paths_fixed) + NEW_LINE  # noqa
+
+    return TEMPLATE.format(title=title, joined_paths=joined)
+
+
+@blueprint.route('/latex', methods=['POST'])
+@login_required
+def create_latex():
+    try:
+        data = request.get_json()
+        paths = data.get('paths')
+        title = data.get('title')
+
+        latex = _latexify(paths, title)
+        return jsonify({'latex': latex}), 200
+    except AttributeError:
+        abort(400, 'Missing data, please provide the list of dataset paths.')
---
+++ tests/integration/test_latex.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of CERN Analysis Preservation Framework.
+# Copyright (C) 2017 CERN.
+#
+# CERN Analysis Preservation Framework is free software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# CERN Analysis Preservation Framework is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CERN Analysis Preservation Framework; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+# or submit itself to any jurisdiction.
+
+"""Integration tests for CAP api."""
+
+from __future__ import absolute_import, print_function
+import json
+
+LATEX_RESULT = '\\begin{array}{ |c| } \\hline\n\\textbf{Paths for Latex} \\\\ ' \
+               '\\hline\n/Electron/Run2010B-PromptReco-v2/RECO \\\\ \\hline\n' \
+               '/EG/Run2010A-Sep17ReReco\\_v2/RECO \\\\ \\hline\n' \
+               '/Photon/Run2010B-PromptReco-v2/RECO \\\\ ' \
+               '\\hline\n\n\\end{array}\n'
+
+
+def test_latex(app, auth_headers_for_superuser, json_headers):
+    mock_data = {
+        'paths': [
+            '/Electron/Run2010B-PromptReco-v2/RECO',
+            '/EG/Run2010A-Sep17ReReco_v2/RECO',
+            '/Photon/Run2010B-PromptReco-v2/RECO'
+        ],
+        'title': 'Paths for Latex'
+    }
+
+    with app.test_client() as client:
+        resp = client.post('services/latex',
+                           headers=auth_headers_for_superuser + json_headers,
+                           data=json.dumps(mock_data))
+
+        assert resp.status_code == 200
+        assert resp.json.get('latex') == LATEX_RESULT
