83464e386255d4b31c5a462d459a1da4b88e5058
papadopan <antonios.papadopan@gmail.com>
2020-01-22 09:43:01 +0100

    ui: schemawizard updates

---
+++ ui/src/reducers/tests/schemaWizard.test.js
@@ -0,0 +1,8 @@
+import schemaReducer from "../schemaWizard";
+import * as actions from "../../actions/schemaWizard";
+
+import { Map, fromJS } from "immutable";
+
+describe("SchemaWizard Reduccer", () => {
+  it("sasa", () => {});
+});
