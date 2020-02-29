f63e70bb810533323963a737199c56f4cf594a53
Pamfilos Fokianos <pamfilosf@gmail.com>
2020-02-29 22:46:28 +0100

    devops: disable commit sibject check
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- scripts/ci/prebuild.sh
+++ scripts/ci/prebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then 
-    ./scripts/helpers/check_commit_subject.sh -r  $TRAVIS_COMMIT_RANGE
+    echo "./scripts/helpers/check_commit_subject.sh -r  $TRAVIS_COMMIT_RANGE"
 fi
