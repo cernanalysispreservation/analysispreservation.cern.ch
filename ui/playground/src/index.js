512330e11ccff1d83d00a37d68c4c32027609b49
papadopan <antonios.papadopan@gmail.com>
2020-04-14 10:33:12 +0200

    ui: new playground workspace

---
+++ ui/playground/src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Helloworld React!</h1>, document.getElementById("root"));
---
+++ ui/playground/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // the output bundle won't be optimized for production but suitable for development
+  mode: "development",
+  // the app entry point is /src/index.js
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    // the output of the webpack build will be in /dist directory
+    path: path.resolve(__dirname, "dist"),
+    // the filename of the JS bundle will be bundle.js
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        // for any file with a suffix of js or jsx
+        test: /\.jsx?$/,
+        // ignore transpiling JavaScript from node_modules as it should be that state
+        exclude: /node_modules/,
+        // use the babel-loader for transpiling JavaScript to a suitable format
+        loader: "babel-loader",
+        options: {
+          // attach the presets to the loader (most projects use .babelrc file instead)
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }
+    ]
+  },
+  // add a custom index.html as the template
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
+};
