9ca64d8e6d3488624af791ac0c38c246cd3378b9
papadopan <antonios.papadopan@gmail.com>
2020-04-23 15:54:01 +0200

    ui: architecture update, move ui folder to cap-react
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/components/errors/404.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Box from "grommet/components/Box";
+import Label from "grommet/components/Button";
+import Anchor from "grommet/components/Anchor";
+import FormPreviousLinkIcon from "grommet/components/icons/base/FormPreviousLink";
+
+const NotFoundPage = () => {
+  return (
+    <Box flex>
+      <Box align="center" full="horizontal" pad="medium" />
+      <Box flex colorIndex="light-2" align="center">
+        <Label>404 Page Not Found</Label>
+      </Box>
+      <Box>
+        <Anchor
+          icon={<FormPreviousLinkIcon />}
+          label="Go back to dashboard"
+          path="/"
+          size="small"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default NotFoundPage;
