c58d313107c771f0a07d711312f36038a84195b1
papadopan <antonios.papadopan@gmail.com>
2020-06-26 10:56:15 +0200

    ui: Search Facets loading updates
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/components/search/SearchFacetLoading/SearchFacetLoading.js
+++ ui/cap-react/src/components/search/SearchFacetLoading/SearchFacetLoading.js
@@ -1,37 +1,33 @@
 import React from "react";
 
-import Sidebar from "grommet/components/Sidebar";
 import Box from "grommet/components/Box";
 
 const SearchFacetLoading = () => {
   return (
-    <Sidebar full={false} colorIndex="light-2" id="sidebar">
-      <Box flex={true} justify="start">
-        <Box flex={true} primary={true} margin={{ vertical: "medium" }}>
-          {[...Array(4)].map((category, index) => {
-            return (
-              <Box key={index}>
-                <Box key={index} margin={{ bottom: "small" }}>
-                  <Box className="search_facet_heading" />
-                  <Box
-                    size="medium"
-                    styles={{ maxHeight: "100px" }}
-                    direction="column"
-                    margin={{ top: "small" }}
-                  >
-                    <Box>
-                      {[...Array(5)].map((field, index) => (
-                        <Box key={index} className="search_facet_listitem" />
-                      ))}
-                    </Box>
+    <Box flex={true} justify="start">
+      <Box flex={true} primary={true} margin={{ vertical: "medium" }}>
+        {[...Array(4)].map((category, index) => {
+          return (
+            <Box key={index} separator="bottom" margin={{ vertical: "medium" }}>
+              <Box key={index} margin={{ bottom: "small" }}>
+                <Box className="search_facet_heading" />
+                <Box
+                  size="medium"
+                  styles={{ maxHeight: "100px" }}
+                  direction="column"
+                >
+                  <Box>
+                    {[...Array(5)].map((field, index) => (
+                      <Box key={index} className="search_facet_listitem" />
+                    ))}
                   </Box>
                 </Box>
               </Box>
-            );
-          })}
-        </Box>
+            </Box>
+          );
+        })}
       </Box>
-    </Sidebar>
+    </Box>
   );
 };
 
