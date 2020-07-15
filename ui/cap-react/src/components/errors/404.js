2f7f96c14391f0f9e162dc7de6a3b137827f5638
papadopan <antonios.papadopan@gmail.com>
2020-07-15 17:11:47 +0200

    ui: update 404 ,403 page
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/components/errors/404.js
+++ ui/cap-react/src/components/errors/404.js
@@ -1,26 +1,56 @@
 import React from "react";
 import Box from "grommet/components/Box";
-import Label from "grommet/components/Button";
-import Anchor from "grommet/components/Anchor";
-import FormPreviousLinkIcon from "grommet/components/icons/base/FormPreviousLink";
+import Heading from "grommet/components/Heading";
+import Label from "grommet/components/Label";
+import Button from "../partials/Button";
+import { withRouter } from "react-router";
+import PropTypes from "prop-types";
 
-const NotFoundPage = () => {
+const NotFoundPage = ({ history }) => {
   return (
-    <Box flex>
-      <Box align="center" full="horizontal" pad="medium" />
-      <Box flex colorIndex="light-2" align="center">
-        <Label>404 Page Not Found</Label>
-      </Box>
-      <Box>
-        <Anchor
-          icon={<FormPreviousLinkIcon />}
-          label="Go back to dashboard"
-          path="/"
-          size="small"
-        />
+    <Box flex colorIndex="light-1" align="center" justify="center">
+      <Box style={{ color: "#006A93" }}>
+        <Box
+          direction="row"
+          align="center"
+          responsive={false}
+          pad="small"
+          style={{ letterSpacing: "1px" }}
+        >
+          <Heading>404</Heading>
+          <Box
+            style={{
+              height: "5px",
+              width: "20px",
+              background: "#006A93",
+              margin: "0 20px"
+            }}
+          />
+          <Heading>Page was not found</Heading>
+        </Box>
+        <Box margin={{ top: "medium" }} align="center">
+          <Label style={{ textAlign: "center" }}>
+            The page your are looking for, either was removed <br />or<br /> the
+            url path is not correct
+          </Label>
+        </Box>
+        <Box
+          direction="row"
+          responsive={false}
+          justify="between"
+          margin={{ top: "large" }}
+          pad="medium"
+        >
+          <Button text="Previous" secondary onClick={() => history.goBack()} />
+          <Button text="Home" primary onClick={() => history.push("/")} />
+        </Box>
       </Box>
     </Box>
   );
 };
 
-export default NotFoundPage;
+NotFoundPage.propTypes = {
+  history: PropTypes.object
+};
+
+export default withRouter(NotFoundPage);
