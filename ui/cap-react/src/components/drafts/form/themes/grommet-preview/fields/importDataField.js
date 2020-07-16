e4b4a5080efc4bdf8866974b7f2d6058571d0782
papadopan <antonios.papadopan@gmail.com>
2020-07-16 16:37:00 +0200

    ui: ImportDataField improve ui
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/components/drafts/form/themes/grommet-preview/fields/importDataField.js
+++ ui/cap-react/src/components/drafts/form/themes/grommet-preview/fields/importDataField.js
@@ -1,27 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Anchor from "grommet/components/Anchor";
+import Anchor from "../../../../../partials/Anchor";
 import Box from "grommet/components/Box";
+import Label from "grommet/components/Label";
+import { AiOutlineLink } from "react-icons/ai";
 
 import axios from "axios";
 
+// schema for the importDataField
+// "statistics_questionnaire": {
+//   "properties": {
+//     "$ref": {
+//       "type": "string"
+//     }
+//   },
+//   "title": "Statistics Questionnare"
+// }
+// uiSchema for the ImportDataField
+// "statistics_questionnaire": {
+//   "ui:field": "ImportDataField",
+//   "ui:options": {
+//     "query": "/api/records/?type=cms-stats-questionnaire-v0.0.1"
+//   },
+//   "ui:object": "accordionObjectField"
+// }
 class ImportDataField extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      link: null
+      link: null,
+      fetched: undefined
     };
   }
 
   componentDidMount() {
-    let { $ref: refLink = null } = this.props.formData;
+    let { $ref: refLink = null, fetched = undefined } = this.props.formData;
+
     if (refLink) {
       axios.get(refLink).then(res => {
         if (res.data && res.data.links) {
           this.setState({
-            link: res.data.links.html
+            link: res.data.links.html,
+            fetched
           });
         }
       });
@@ -29,16 +51,20 @@ class ImportDataField extends React.Component {
   }
 
   render() {
-    return (
-      this.state.link && (
-        <Box wrap={true} pad="none">
-          <Anchor
-            label="view"
-            href={this.state.link}
-            style={{ textDecoration: "none" }}
-          />
-        </Box>
-      )
+    return this.state.link ? (
+      <Box wrap={true} pad={{ horizontal: "medium" }}>
+        <Anchor target="_blank" href={this.state.link}>
+          <Box direction="row" align="center" responsive={false}>
+            <Label size="small" style={{ marginRight: "5px" }}>
+              {this.state.fetched ? this.state.fetched.id : "View"}
+            </Label>
+
+            <AiOutlineLink />
+          </Box>
+        </Anchor>
+      </Box>
+    ) : (
+      <Box pad={{ horizontal: "medium" }}>-</Box>
     );
   }
 }
--- ui/cap-react/src/components/drafts/form/themes/grommet/fields/importDataField.js
+++ ui/cap-react/src/components/drafts/form/themes/grommet/fields/importDataField.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Select from "grommet/components/Select";
+import Select from "react-select";
+import Box from "grommet/components/Box";
 
 import axios from "axios";
+
+import CustomOption from "./ImportDataFieldSelectComponent";
+
+const EMPTY_VALUE = "---- No Selection ---- ";
 
 class ImportDataField extends React.Component {
   constructor(props) {
@@ -19,21 +24,31 @@ class ImportDataField extends React.Component {
     const query = this.props.uiSchema["ui:options"].query || undefined;
     let selected = this.state.selected;
 
+    // check if there is saved selected value
+    if (this.props.formData && this.props.formData.fetched) {
+      selected = this.props.formData.fetched.label;
+    }
+
+    // query to fetch options for the select menu
     if (query) {
       axios.get(query).then(res => {
         let results = res.data.hits.hits.map(item => {
           let _item = {
             value: item.links.self,
-            label: item.metadata.general_title || item.id
+            label: item.metadata.general_title || item.id,
+            id: item.id,
+            created: item.created
           };
-          if (item.links.self === this.props.formData) selected = _item.label;
+
           return _item;
         });
 
+        // add one more item in order to help users clear theis choice easier
         results.unshift({
           value: undefined,
-          label: "none"
-        }); // adds option to remove selection
+          label: EMPTY_VALUE,
+          noSelection: true
+        });
 
         this.setState({
           data: results,
@@ -43,21 +58,33 @@ class ImportDataField extends React.Component {
     }
   }
 
-  _saveSelection = ({ option }) => {
-    this.setState({ selected: option.label }, () =>
-      this.props.onChange(option.value)
-    );
-  };
-
   render() {
     return (
-      <Select
-        options={this.state.data}
-        onChange={this._saveSelection}
-        value={this.state.selected}
-        placeHolder="Pick created statistics questionnaire"
-        style={{ height: "3.4em" }}
-      />
+      <Box pad={{ horizontal: "medium" }}>
+        <Select
+          isClearable
+          onChange={data => {
+            if (!data || !data.value) {
+              this.setState({ selected: undefined }, () => {
+                this.props.onChange({ $ref: undefined, fetched: undefined });
+              });
+            } else
+              this.setState({ selected: data.label }, () =>
+                this.props.onChange({ $ref: data.value, fetched: data })
+              );
+          }}
+          options={this.state.data}
+          components={{
+            Option: CustomOption
+          }}
+          placeholder={"Select a Statistic Questionnaire"}
+          value={
+            this.state.selected && this.state.selected !== EMPTY_VALUE
+              ? { label: this.state.selected, value: this.state.selected }
+              : null
+          }
+        />
+      </Box>
     );
   }
 }
