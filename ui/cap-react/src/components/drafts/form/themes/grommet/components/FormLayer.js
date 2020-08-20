fe14728e8b05d31a566b7853633621b98c771d1f
papadopan <antonios.papadopan@gmail.com>
2020-08-20 10:01:13 +0300

    ui: remove Provider from FormLayer

--- ui/cap-react/src/components/drafts/form/themes/grommet/components/FormLayer.js
+++ ui/cap-react/src/components/drafts/form/themes/grommet/components/FormLayer.js
@@ -7,9 +7,6 @@ import Button from "grommet/components/Button";
 
 import Trash from "grommet/components/icons/base/Trash";
 
-import { Provider } from "react-redux";
-import store from "../../../../../../store/configureStore";
-
 import Modal from "../../../../../partials/Modal";
 
 class FormLayer extends React.Component {
@@ -25,37 +22,31 @@ class FormLayer extends React.Component {
   render() {
     return this.props.layerActive ? (
       <Modal position="right" onClose={this.props.onClose} Tag="h4" full>
-        <Provider store={store}>
-          <Box justify="center" align="center" pad={{ horizontal: "small" }}>
-            <Box size="large" pad={{ horizontal: "large" }}>
-              <Box>{this.props.properties}</Box>
+        <Box justify="center" align="center" pad={{ horizontal: "small" }}>
+          <Box size="large" pad={{ horizontal: "large" }}>
+            <Box>{this.props.properties}</Box>
 
-              <Box
-                direction="row"
-                justify="between"
-                pad={{ vertical: "small" }}
-              >
-                <Box>
+            <Box direction="row" justify="between" pad={{ vertical: "small" }}>
+              <Box>
+                <Button
+                  label="OK"
+                  primary={true}
+                  onClick={this.props.onClose}
+                />
+              </Box>
+              <Box>
+                {this.props.remove ? (
                   <Button
-                    label="OK"
-                    primary={true}
-                    onClick={this.props.onClose}
+                    label="Remove"
+                    plain={true}
+                    onClick={this.removeAndClose}
+                    icon={<Trash />}
                   />
-                </Box>
-                <Box>
-                  {this.props.remove ? (
-                    <Button
-                      label="Remove"
-                      plain={true}
-                      onClick={this.removeAndClose}
-                      icon={<Trash />}
-                    />
-                  ) : null}
-                </Box>
+                ) : null}
               </Box>
             </Box>
           </Box>
-        </Provider>
+        </Box>
       </Modal>
     ) : null;
   }
