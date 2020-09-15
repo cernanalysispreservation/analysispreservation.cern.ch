709673770dd7561ef2462f0bbdb53b0093f6957a
Ilias Koutsakis <ilias.koutsakis@cern.ch>
2020-09-15 18:04:01 +0200

    global: improve setup.py for installation
    * pipenv install
    * go into the virtualenv
    * pip install -e .
    
    Signed-off-by: Ilias Koutsakis <ilias.koutsakis@cern.ch>

--- setup.py
+++ setup.py
@@ -19,12 +19,11 @@ INVENIO_VERSION = '3.0.0'  # "3.0.0rc2"
 
 tests_require = [
     'check-manifest>=0.35',
-    'coverage>=4.4.1',
+    'coverage<5.0.0,>=4.0',
     'isort>=4.3',
     'mock>=2.0.0',
     'pydocstyle>=2.0.0',
     'pytest-cov>=2.5.1',
-    'pytest-invenio>=1.0.5',
     'pytest-invenio>=1.0.5,<=1.3.4',
     'pytest-mock>=1.6.0',
     'pytest-pep8>=1.0.6',
@@ -35,27 +34,32 @@ tests_require = [
     'selenium>=3.4.3',
 ]
 
-extras_require = {'docs': ['Sphinx>=1.5.1'], 'tests': tests_require}
+extras_require = {
+    'ldap': ['python-ldap>=2.4.39'],
+    'xrootd': [
+        # 'invenio-xrootd>=1.0.0a6',
+        # 'xrootdpyfs>=0.1.5',
+    ],
+    'tests': tests_require,
+    'all': []
+}
 
-extras_require['all'] = []
 for reqs in extras_require.values():
     extras_require['all'].extend(reqs)
 
-extras_require['ldap'] = ['python-ldap>=2.4.39']
-
-# Do not include in all requirement
-extras_require['xrootd'] = [
-    # 'invenio-xrootd>=1.0.0a6',
-    # 'xrootdpyfs>=0.1.5',
+setup_requires = [
+    'Babel>=2.4.0',
+    'pytest-runner>=3.0.0,<5'
 ]
-
-setup_requires = ['Babel>=2.4.0', 'pytest-runner>=3.0.0,<5']
 
 install_requires = [
     'Flask==0.12.4',
     'Flask-Cli>=0.4.0',
     'Flask-Cache>=0.13.1',
     'Flask-Debugtoolbar>=0.10.1',
+    'Flask-Breadcrumbs~=0.4.0',
+    'flask-shell-ipython~=0.3.1',
+    'flask-login<0.5.0,>=0.4.1',
 
     # CAP specific libraries
     'PyGithub>=1.35',
@@ -68,28 +72,68 @@ install_requires = [
     # Pinned libraries
     'urllib3[secure]==1.22',
     'sqlalchemy==1.3.0',
-    # temporary pinned since there are 'fs' conslicts between
+    # temporary pinned since there are 'fs' conflicts between
     # 'reana-commons' and 'invenio-files-rest'
     'fs==0.5.4',
     'invenio-accounts-rest>=1.0.0a4',
-    'invenio-oauthclient>=1.0.0',
-    'invenio-userprofiles>=1.0.0',
     'invenio-query-parser>=0.3.0',
-    'invenio[{db},{es},base,auth,metadata]~={version}'.format(
+    'invenio[{db},{es},base,auth,metadata]=={version}'.format(
         db=DATABASE, es=ELASTICSEARCH, version=INVENIO_VERSION),
     'invenio-rest==1.0.0',
     'invenio-files-rest==1.0.0',
     'invenio-records-files==1.0.0a11',
     'coolname==1.1.0',
     'Authlib==0.12.1',
-    # 'invenio-logging' < v1.2.0 is needed because of 'invenio-base' version
-    # conflicts, USING 'cernanalysispreservation/invenio-logging' FORM now
-    # 'invenio-logging[sentry, sentry-sdk]<=1.2.0',
     'uWSGI==2.0.17',
     'uwsgi-tools==1.1.1',
     'uwsgitop==0.10',
     # needed version for future use of arguments
     'webargs==3.0.1',
+    'psycopg2-binary>=2.7.4',
+    'werkzeug<=0.16.4,>=0.14.1',
+    'jsonresolver<0.3.0,>=0.2.1',
+    'backports.functools-lru-cache==1.5',
+    'oauthlib==2.1.0',
+    'email_validator',
+    'pandas',
+    'reana-client==0.6.0',
+    'six==1.12.0',
+    'SQLAlchemy-Utils==0.33.3',
+    'cookiecutter==1.6.0',
+
+    # 'invenio-logging' < v1.2.0 is needed because of 'invenio-base' version
+    # conflicts, USING 'cernanalysispreservation/invenio-logging' FORM now
+    # 'invenio-logging[sentry, sentry-sdk]<=1.2.0',
+
+    'invenio-access==1.0.1',
+    'invenio-accounts==1.0.1',
+    'invenio-admin==1.0.0',
+    'invenio-app==1.0.0',
+    'invenio-base==1.0.1',
+    'invenio-config<1.1.0,>=1.0.1',
+    'invenio-db<1.0.5,>=1.0.1',
+    'invenio-oauth2server==1.0.1',
+    'invenio-oauthclient==1.0.0',
+    'invenio-formatter==1.0.3',
+    'invenio-indexer==1.0.1',
+    'invenio-records-rest==1.1.0',
+
+    'invenio-deposit @ https://github.com/annatrz/invenio-deposit/archive/master.zip#egg=invenio-deposit',
+    'invenio-oauthclient @ '
+    'https://github.com/cernanalysispreservation/invenio-oauthclient/archive/master.zip#egg=invenio-oauthclient',
+    'invenio-jsonschemas @ '
+    'https://github.com/cernanalysispreservation/invenio-jsonschemas/archive/master.zip#egg=invenio-jsonschemas',
+    'invenio-search @ '
+    'https://github.com/cernanalysispreservation/invenio-search/archive/master.zip#egg=invenio-search',
+    'invenio-userprofiles @ '
+    'https://github.com/cernanalysispreservation/invenio-userprofiles/archive/master.zip#egg=invenio-userprofiles',
+    'invenio-logging @ '
+    'https://github.com/cernanalysispreservation/invenio-logging/archive/v1.2.0-without-invenio-base-des.zip#egg=invenio-logging[sentry,sentry-sdk]',  # noqa
+    'flask-celeryext @ '
+    'https://github.com/Lilykos/flask-celeryext/archive/master.zip#egg=flask-celeryext',
+    'invenio-records @ '
+    'https://github.com/inveniosoftware/invenio-records/archive/a5d4efb1e1466aa571aeef013d699dca1452f7cd.zip#egg=invenio-records',  # noqa
+    'celery @ https://github.com/celery/celery/archive/6ccdc7b9f8e02d21275e923dccc7ccb9185e6153.zip#egg=celery',
 ]
 
 packages = find_packages()
@@ -97,7 +141,7 @@ packages = find_packages()
 # Get the version string. Cannot be done with import!
 g = {}
 with open(os.path.join('cap', 'version.py'), 'rt') as fp:
-    exec (fp.read(), g)
+    exec(fp.read(), g)
     version = g['__version__']
 
 setup(
@@ -110,7 +154,7 @@ setup(
     author='CERN',
     author_email='analysis-preservation-support@cern.ch',
     url=
-    'https://github.com/cernanalysispreservation/analysispreservation.cern.ch',  # noqa
+    'https://github.com/cernanalysispreservation/analysispreservation.cern.ch',
     packages=packages,
     zip_safe=False,
     include_package_data=True,
