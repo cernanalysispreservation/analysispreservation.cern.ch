2217505035cf51d75762e3be4fe7fb2217e023f0
Ilias Koutsakis <ilias.koutsakis@cern.ch>
2020-10-14 00:03:11 +0200

    services: ping fixes and tests
    Signed-off-by: Ilias Koutsakis <ilias.koutsakis@cern.ch>

---
+++ tests/integration/test_service_pings.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of CERN Analysis Preservation Framework.
+# Copyright (C) 2016 CERN.
+#
+# CERN Analysis Preservation Framework is free software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# CERN Analysis Preservation Framework is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CERN Analysis Preservation Framework; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Integration tests for pings."""
+
+
+def test_ping(app):
+    with app.test_client() as client:
+        resp = client.get('ping')
+
+        assert resp.status_code == 200
+        assert resp.data == b'Pong!'
+
+
+def test_ping_db(app, location):
+    with app.test_client() as client:
+        resp = client.get('ping/db')
+
+        assert resp.status_code == 200
+        assert resp.data == b'OK!'
+
+
+def test_ping_files(app, location):
+    with app.test_client() as client:
+        resp = client.get('ping/files')
+
+        assert resp.status_code == 500
+        assert resp.data == b'ERROR'
+
+
+def test_ping_search(app):
+    with app.test_client() as client:
+        resp = client.get('ping/search')
+
+        assert resp.status_code == 200
+        assert resp.data == b'OK!'
