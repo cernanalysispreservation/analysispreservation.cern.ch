c385d45ad1684370d972593f0838fb3547a108ec
papadopan <antonios.papadopan@gmail.com>
2020-11-25 15:52:06 +0100

    ui: form builder update the cmsindex ui
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/components/cms/components/CMSIndex/DropZoneForm.js
+++ ui/cap-react/src/components/cms/components/CMSIndex/DropZoneForm.js
@@ -7,6 +7,9 @@ import { Paragraph } from "grommet";
 import cogoToast from "cogo-toast";
 import { connect } from "react-redux";
 import { initSchemaWizard } from "../../../../actions/schemaWizard";
+import Header from "grommet/components/Header";
+
+import Heading from "grommet/components/Heading";
 
 const DropZoneForm = props => {
   const isfileJson = filename => {
@@ -19,67 +22,71 @@ const DropZoneForm = props => {
   };
 
   return (
-    <Box>
-      <Dropzone
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          flex: 1,
-          width: "100%",
-          height: "100px",
-          padding: "30px 10px",
-          border: "2px dotted rgba(0, 0, 0, 0.25)",
-          borderRadius: "4px",
-          justifyContent: "center",
-          alignContent: "center",
-          alignItems: "center",
-          cursor: "pointer"
-        }}
-        multiple={false}
-        onDrop={file => {
-          if (file.length > 0) {
-            const acceptedFile = isfileJson(file[0].name);
+    <Box size="medium">
+      <Header justify="center">
+        <Heading tag="h3">Import your own JSON </Heading>
+      </Header>
+      <Box pad="medium">
+        <Dropzone
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            flex: 1,
+            height: "100px",
+            padding: "30px 10px",
+            border: "2px dotted rgba(0, 0, 0, 0.25)",
+            borderRadius: "4px",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            cursor: "pointer"
+          }}
+          multiple={false}
+          onDrop={file => {
+            if (file.length > 0) {
+              const acceptedFile = isfileJson(file[0].name);
 
-            if (acceptedFile) {
-              let reader = new FileReader();
-              reader.onload = function(event) {
-                const newSchema = JSON.parse(event.target.result);
-                if (
-                  newSchema["deposit_schema"] &&
-                  newSchema["deposit_options"]
-                ) {
-                  props.initWizard(newSchema);
-                } else {
-                  cogoToast.error(
-                    "Your json should include a deposit_schema and a deposit_option key",
-                    {
-                      position: "top-center",
-                      heading: "Missing Keys",
-                      bar: { size: "0" },
-                      hideAfter: 3
-                    }
-                  );
-                }
-              };
-              reader.readAsText(file[0]);
-            } else {
-              cogoToast.error("Your file format should be json", {
-                position: "top-center",
-                bar: { size: "0" },
-                hideAfter: 3,
-                heading: "File Format"
-              });
+              if (acceptedFile) {
+                let reader = new FileReader();
+                reader.onload = function(event) {
+                  const newSchema = JSON.parse(event.target.result);
+                  if (
+                    newSchema["deposit_schema"] &&
+                    newSchema["deposit_options"]
+                  ) {
+                    props.initWizard(newSchema);
+                  } else {
+                    cogoToast.error(
+                      "Your json should include a deposit_schema and a deposit_option key",
+                      {
+                        position: "top-center",
+                        heading: "Missing Keys",
+                        bar: { size: "0" },
+                        hideAfter: 3
+                      }
+                    );
+                  }
+                };
+                reader.readAsText(file[0]);
+              } else {
+                cogoToast.error("Your file format should be json", {
+                  position: "top-center",
+                  bar: { size: "0" },
+                  hideAfter: 3,
+                  heading: "File Format"
+                });
+              }
             }
-          }
-        }}
-      >
-        <Box flex={false} direction="row" align="center">
-          <FormUploadIcon size="small" />
-          <Paragraph margin="none">Browse files</Paragraph>
-        </Box>
-        <Paragraph margin="small">OR</Paragraph>
-        <Paragraph margin="none">Drop your JSON file here</Paragraph>
-      </Dropzone>
+          }}
+        >
+          <Box flex={false} direction="row" align="center">
+            <FormUploadIcon size="small" />
+            <Paragraph margin="none">Browse files</Paragraph>
+          </Box>
+          <Paragraph margin="small">OR</Paragraph>
+          <Paragraph margin="none">Drop your JSON file here</Paragraph>
+        </Dropzone>
+      </Box>
     </Box>
   );
 };
@@ -87,7 +94,7 @@ const DropZoneForm = props => {
 DropZoneForm.propTypes = {
   initWizard: PropTypes.func
 };
-const mapStateToProps = state => ({});
+const mapStateToProps = () => ({});
 
 const mapDispatchToProps = dispatch => ({
   initWizard: data => dispatch(initSchemaWizard(data))
