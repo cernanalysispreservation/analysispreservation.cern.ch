f3f482cd38cce196753eea061198d018d416d421
papadopan <antonios.papadopan@gmail.com>
2021-02-10 15:51:27 +0100

    ui: fix EditableField close func
    * closes #2081
    
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/components/partials/EditableField.js
+++ ui/cap-react/src/components/partials/EditableField.js
@@ -7,7 +7,7 @@ import Label from "grommet/components/Label";
 import CheckmarkIcon from "grommet/components/icons/base/Checkmark";
 import CloseIcon from "grommet/components/icons/base/Close";
 
-import {AiOutlineEdit} from "react-icons/ai";
+import { AiOutlineEdit } from "react-icons/ai";
 
 class EditableField extends React.Component {
   constructor(props) {
@@ -66,7 +66,8 @@ class EditableField extends React.Component {
   _unedit = () => {
     this.setState({
       hoverTitle: false,
-      editTitle: false
+      editTitle: false,
+      value: this.props.value || ""
     });
   };
 
