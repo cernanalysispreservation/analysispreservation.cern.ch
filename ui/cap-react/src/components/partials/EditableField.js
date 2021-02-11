ce8839c6df87db1c57dc76488a141e606a813d28
papadopan <antonios.papadopan@gmail.com>
2021-02-11 16:56:53 +0100

    ui: EditableField redo
    * closes #2088
    * closes #2091
    
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/components/partials/EditableField.js
+++ ui/cap-react/src/components/partials/EditableField.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import Box from "grommet/components/Box";
@@ -9,140 +9,122 @@ import CloseIcon from "grommet/components/icons/base/Close";
 
 import { AiOutlineEdit } from "react-icons/ai";
 
-class EditableField extends React.Component {
-  constructor(props) {
-    super(props);
+const EditableField = ({
+  value,
+  size = null,
+  emptyValue = "Untitled Document",
+  renderDisplay = null,
+  onUpdate = null,
+  isEditable = true
+}) => {
+  const [currentValue, setCurrentValue] = useState(value);
+  const [hoverTitle, setHoverTitle] = useState(false);
+  const [editTitle, setEditTitle] = useState(false);
 
-    this.state = {
-      editTitle: false,
-      hoverTitle: false,
-      emptyValue: this.props.emptyValue || "no value",
-      value: this.props.value || ""
-    };
-  }
-
-  _focusInput = () => {
-    this.setState({
-      editTitle: true
-    });
+  const _onChange = e => {
+    setCurrentValue(e.target.value);
   };
 
-  _hoverIn = () => {
-    this.setState({ hoverTitle: true });
-  };
-
-  _hoverOut = () => {
-    this.setState({ hoverTitle: false });
-  };
-
-  _onChange = e => {
-    this.setState({ value: e.target.value });
-  };
-
-  _update = () => {
-    if (this.props.onUpdate) {
-      this.props.onUpdate(this.state.value);
+  const _update = () => {
+    if (onUpdate) {
+      onUpdate(currentValue);
     }
-
-    this.setState({
-      hoverTitle: false,
-      editTitle: false
-    });
+    setHoverTitle(true);
+    setEditTitle(false);
   };
 
-  _unedit = () => {
-    this.setState({
-      hoverTitle: false,
-      editTitle: false,
-      value: this.props.value || ""
-    });
+  const _unedit = () => {
+    setHoverTitle(false);
+    setEditTitle(false);
   };
 
-  handleKeyDown = e => {
-    if (e.key === "Enter") this._update();
-    if (e.key === "Escape") this._unedit();
+  const handleKeyDown = e => {
+    if (e.key === "Enter") _update();
+    if (e.key === "Escape") _unedit();
   };
 
-  render() {
-    return this.state.editTitle ? (
-      <Box
-        flex={true}
-        direction="row"
-        wrap={false}
-        pad="none"
-        responsive={false}
-        className="jst-md-center"
-      >
-        <Label size={this.props.size || null} margin="none" direction="row">
-          <input
-            key="draft-input"
-            style={{ padding: 0, border: "1px solid #136994", borderRadius: 0 }}
-            onChange={this._onChange}
-            value={this.state.value}
-            autoFocus={true}
-            onKeyDown={this.handleKeyDown}
-            onClick={e => e.stopPropagation()}
-          />
-        </Label>
-        <Box margin="none" direction="row" align="center" responsive={false}>
-          <Box
-            pad={{ horizontal: "small" }}
-            margin="none"
-            onClick={this._update.bind(this)}
-          >
-            <CheckmarkIcon colorIndex="neutral-1" size="xsmall" />
-          </Box>
-          <Box margin="none" onClick={this._unedit.bind(this)}>
-            <CloseIcon colorIndex="neutral-1" size="xsmall" />
-          </Box>
+  return editTitle ? (
+    <Box
+      flex={true}
+      direction="row"
+      wrap={false}
+      pad="none"
+      responsive={false}
+      className="jst-md-center"
+    >
+      <Label size={size} margin="none" direction="row">
+        <input
+          key="draft-input"
+          style={{ padding: 0, border: "1px solid #136994", borderRadius: 0 }}
+          onChange={_onChange}
+          value={currentValue}
+          autoFocus={true}
+          onKeyDown={handleKeyDown}
+          onClick={e => e.stopPropagation()}
+        />
+      </Label>
+      <Box margin="none" direction="row" align="center" responsive={false}>
+        <Box
+          pad={{ horizontal: "small" }}
+          margin="none"
+          onClick={() => _update()}
+        >
+          <CheckmarkIcon colorIndex="neutral-1" size="xsmall" />
+        </Box>
+        <Box margin="none" onClick={() => _unedit()}>
+          <CloseIcon colorIndex="neutral-1" size="xsmall" />
         </Box>
       </Box>
-    ) : (
-      <Box flex={true} direction="row" wrap={false}>
-        <Box
-          align="center"
-          key="draft-title"
-          direction="row"
-          responsive={false}
-          wrap={false}
-          onMouseEnter={this._hoverIn}
-          onMouseLeave={this._hoverOut}
-          onClick={this._focusInput}
-          pad={{ between: "small" }}
-          style={{
-            padding: "0 2px",
-            border: this.state.hoverTitle
+    </Box>
+  ) : (
+    <Box flex={true} direction="row" wrap={false}>
+      <Box
+        align="center"
+        key="draft-title"
+        direction="row"
+        responsive={false}
+        wrap={false}
+        onMouseEnter={() => setHoverTitle(true)}
+        onMouseLeave={() => setHoverTitle(false)}
+        onClick={
+          isEditable
+            ? () => {
+                setCurrentValue(value);
+                setEditTitle(true);
+              }
+            : null
+        }
+        pad={{ between: "small" }}
+        style={{
+          padding: "0 2px",
+          border:
+            hoverTitle && isEditable
               ? "1px solid #136994"
               : "1px solid transparent"
-          }}
-        >
-          <Box flex={true}>
-            {this.props.renderDisplay ? (
-              this.props.renderDisplay(this.state.value)
-            ) : (
-              <Label
-                size={this.props.size || null}
-                align="start"
-                pad="none"
-                margin="none"
-              >
-                {this.state.value ? this.state.value : this.state.emptyValue}
-              </Label>
-            )}
-          </Box>
-          <AiOutlineEdit size="10" />
+        }}
+      >
+        <Box flex={true}>
+          {renderDisplay ? (
+            renderDisplay(this.state.value)
+          ) : (
+            <Label size={size} align="start" pad="none" margin="none">
+              {value || emptyValue}
+            </Label>
+          )}
         </Box>
+        {isEditable && <AiOutlineEdit size="15" />}
       </Box>
-    );
-  }
-}
+    </Box>
+  );
+};
 
 EditableField.propTypes = {
   renderDisplay: PropTypes.func,
   emptyValue: PropTypes.string,
   value: PropTypes.string,
   onUpdate: PropTypes.func,
-  size: PropTypes.string
+  size: PropTypes.string,
+  isEditable: PropTypes.bool
 };
 
 export default EditableField;
