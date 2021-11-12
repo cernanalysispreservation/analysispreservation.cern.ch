d8e9086d4252c3461d148ed126bf5c0b4ba8889f
papadopan <antonios.papadopan@gmail.com>
2021-11-12 10:44:45 +0100

    ui: intro to ant design
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/partials/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Row, Typography, Space } from "antd";
+import { Link } from "react-router-dom";
+
+const { Text } = Typography;
+
+const Footer = () => {
+  return (
+    <Row
+      align="bottom"
+      justify="center"
+      style={{ padding: "5px", background: "#001529" }}
+    >
+      <Space direction="horizontal" size="middle">
+        <Text style={{ color: "#fff" }}>
+          Copyright {new Date().getFullYear()} © CERN. Created & Hosted by CERN.
+          Powered by Invenio Software.
+        </Text>
+        <Link to="/about">About</Link>
+        <Link to="/policy">Policy</Link>
+      </Space>
+    </Row>
+  );
+};
+
+Footer.propTypes = {};
+
+export default Footer;
