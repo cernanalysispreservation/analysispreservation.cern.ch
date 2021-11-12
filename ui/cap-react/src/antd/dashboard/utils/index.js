7b6798dd5e7d8140df8b4fd549eec3df3a520bdb
papadopan <antonios.papadopan@gmail.com>
2021-11-12 10:44:45 +0100

    ui: intro to ant design
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/dashboard/utils/index.js
@@ -0,0 +1,30 @@
+export const _getList = results => {
+  return {
+    drafts: {
+      all: {
+        list: results.drafts.data,
+        more: results.drafts.more
+      },
+      yours: {
+        list: results.user_drafts.data,
+        more: results.user_drafts.more
+      }
+    },
+    published: {
+      all: {
+        list: results.published.data,
+        more: results.published.more
+      },
+      yours: {
+        list: results.user_published.data,
+        more: results.user_published.more
+      }
+    },
+    workflows: {
+      yours: {
+        list: results.user_workflows.data,
+        more: results.user_workflows.more
+      }
+    }
+  };
+};
