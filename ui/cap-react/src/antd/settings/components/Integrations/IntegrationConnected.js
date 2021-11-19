27d816db9745521d4371fcd2441ed0f31b7ccd10
papadopan <antonios.papadopan@gmail.com>
2021-11-19 19:01:30 +0100

    ui: ant settings page
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/settings/components/Integrations/IntegrationConnected.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, Col, Row, Space, Typography } from "antd";
+import { LinkOutlined } from "@ant-design/icons";
+
+const IntegrationConnected = ({ name, data, onClick }) => {
+  return ["github", "gitlab"].includes(name) ? (
+    <Row justify="center">
+      <Col xs={{ span: 16 }} style={{ textAlign: "center" }}>
+        <Space style={{ width: "100%" }} direction="vertical" size="large">
+          <Button
+            icon={<LinkOutlined />}
+            type="link"
+            target="_blank"
+            href={data.profile_url}
+          >
+            {data.name}
+          </Button>
+
+          <Button type="secondary" onClick={onClick}>
+            Disconnect
+          </Button>
+        </Space>
+      </Col>
+    </Row>
+  ) : (
+    <Row justify="center">
+      <Col xs={{ span: 20 }} style={{ textAlign: "center" }}>
+        <Space style={{ width: "100%" }} direction="vertical" size="large">
+          <Typography.Title level={5}>{data.name}</Typography.Title>
+          <Button type="secondary" onClick={onClick}>
+            Disconnect
+          </Button>
+        </Space>
+      </Col>
+    </Row>
+  );
+};
+
+IntegrationConnected.propTypes = {
+  name: PropTypes.string,
+  data: PropTypes.object,
+  onClick: PropTypes.func
+};
+
+export default IntegrationConnected;
---
+++ ui/cap-react/src/antd/settings/components/Integrations/Integrations.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Card, Row, Col, Tag, Typography, Space } from "antd";
+import AuthPopUp from "./IntegrationPopUp";
+import { CheckCircleOutlined } from "@ant-design/icons";
+import IntegrationConnected from "./IntegrationConnected";
+import { services } from "../../utils/services";
+
+const Integrations = ({
+  updateIntegrations,
+  integrations,
+  removeIntegrations
+}) => {
+  return (
+    <Card title="Integrations with other services">
+      <Row gutter={[6, 6]}>
+        {services.map(item => (
+          <Col key={item} xs={24} md={12}>
+            <Card
+              title={item.label}
+              extra={
+                integrations &&
+                integrations[item.name] && (
+                  <Tag color="green" icon={<CheckCircleOutlined />}>
+                    Connected
+                  </Tag>
+                )
+              }
+            >
+              {integrations && integrations[item.name] ? (
+                <IntegrationConnected
+                  name={item.name}
+                  data={integrations[item.name]}
+                  onClick={() => removeIntegrations(item.name)}
+                />
+              ) : (
+                <Row>
+                  <Col style={{ textAlign: "center" }} xs={{ span: 24 }}>
+                    <Space
+                      style={{ width: "100%" }}
+                      direction="vertical"
+                      size="large"
+                    >
+                      <Typography.Title level={5}>
+                        Connect to {item.label}
+                      </Typography.Title>
+
+                      <AuthPopUp
+                        service={item.name}
+                        loginCallBack={() => updateIntegrations()}
+                      />
+                    </Space>
+                  </Col>
+                </Row>
+              )}
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Card>
+  );
+};
+
+Integrations.propTypes = {
+  updateIntegrations: PropTypes.func,
+  integrations: PropTypes.object,
+  removeIntegrations: PropTypes.func
+};
+
+export default Integrations;
