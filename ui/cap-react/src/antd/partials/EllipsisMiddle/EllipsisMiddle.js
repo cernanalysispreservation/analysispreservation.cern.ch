934b3e7cf54903e9079d2ead215ce9469e4966ca
papadopan <antonios.papadopan@gmail.com>
2021-11-19 19:01:30 +0100

    ui: ant settings page
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/partials/EllipsisMiddle/EllipsisMiddle.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Typography } from "antd";
+
+const EllipsisMiddle = ({ suffixCount, children, copyable }) => {
+  const start = children.slice(0, children.length - suffixCount).trim();
+  const suffix = children.slice(-suffixCount).trim();
+  return (
+    <Typography.Text
+      style={{ maxWidth: "100%" }}
+      ellipsis={{ suffix }}
+      copyable={copyable}
+    >
+      {start}
+    </Typography.Text>
+  );
+};
+
+EllipsisMiddle.propTypes = {
+  copyable: PropTypes.bool,
+  children: PropTypes.string,
+  suffixCount: PropTypes.number
+};
+
+export default EllipsisMiddle;
