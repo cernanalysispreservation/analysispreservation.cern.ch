7f50a63230aee02fd01585eec2a061b57552c2ca
papadopan <antonios.papadopan@gmail.com>
2021-11-29 17:05:55 +0100

    ui: ant drafts settings page
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/partials/Reviews/ReviewsContainer.js
@@ -0,0 +1,31 @@
+import { connect } from "react-redux";
+import Reviews from "./Reviews";
+import { reviewDraft } from "../../../actions/draftItem";
+import { reviewPublished } from "../../../actions/published";
+
+const mapStateToProps = (state, props) => ({
+  review: props.isReviewingPublished
+    ? state.published.get("review")
+    : state.draftItem.get("review"),
+  canReview: props.isReviewingPublished
+    ? null
+    : state.draftItem.get("can_review"),
+  draft_id: state.draftItem.get("id"),
+  draftLoading: state.draftItem.get("loading"),
+  draftReviewError: state.draftItem.get("reviewError"),
+  publishedReviewError: state.published.get("reviewError"),
+  shouldDisplayButtonsWhenFromPublished:
+    props.isReviewingPublished && state.published.get("can_review")
+});
+
+const mapDispatchToProps = dispatch => ({
+  reviewDraft: (draft_id, review, message) =>
+    dispatch(reviewDraft(draft_id, review, message)),
+  reviewPublished: (review, message) =>
+    dispatch(reviewPublished(review, message))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Reviews);
