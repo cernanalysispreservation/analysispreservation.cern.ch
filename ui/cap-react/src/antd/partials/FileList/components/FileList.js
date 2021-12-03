305adf0a8cfc7c90433484665c09eca214c9c89e
papadopan <antonios.papadopan@gmail.com>
2021-12-03 14:12:56 +0100

    ui: ant draft sidebar
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/partials/FileList/components/FileList.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { filter } from "lodash";
+import HorizontalWithText from "../../LineText";
+import { Empty, Space, Tree } from "antd";
+import { constructTree } from "../utils/fileList";
+
+const FileList = ({
+  files,
+  renderList = ["files", "repositories", "title"]
+}) => {
+  const [data, setData] = useState(constructTree(files));
+
+  useEffect(
+    () => {
+      setData(constructTree(files));
+    },
+    [files]
+  );
+
+  const getContentFromProps = (value, displayTitle, repos, files) => {
+    const choices = {
+      repositories: (
+        <React.Fragment>
+          {displayTitle && <HorizontalWithText text="All Repositories" />}
+          {repos && repos.length > 0 ? (
+            <Tree.DirectoryTree
+              treeData={repos[0].children || []}
+              selectable={false}
+            />
+          ) : (
+            <Empty description="No repos uploaded yet" />
+          )}
+        </React.Fragment>
+      ),
+      files: (
+        <React.Fragment>
+          <Space direction="vertical" style={{ width: "100%" }}>
+            {displayTitle && <HorizontalWithText text="All Files" />}
+            {files.children && files.children.length > 0 ? (
+              <Tree.DirectoryTree
+                treeData={files.children}
+                selectable={false}
+              />
+            ) : (
+              <Empty description="No files uploaded yet" />
+            )}
+          </Space>
+        </React.Fragment>
+      )
+    };
+
+    return choices[value];
+  };
+
+  let repos = filter(data.children, { name: "repositories" });
+  let allFiles = {
+    children: filter(data.children, item => item.name != "repositories")
+  };
+  let displayTitle = renderList.includes("title");
+
+  return (
+    <Space direction="vertical" style={{ width: "100%" }} size="large">
+      {renderList.map(item =>
+        getContentFromProps(item, displayTitle, repos, allFiles)
+      )}
+    </Space>
+  );
+};
+
+FileList.propTypes = {
+  renderList: PropTypes.array,
+  files: PropTypes.array
+};
+
+export default FileList;
