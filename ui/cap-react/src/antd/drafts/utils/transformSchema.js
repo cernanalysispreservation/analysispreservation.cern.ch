32693c689e3fb0e30915b6922f154925a4c825ce
papadopan <antonios.papadopan@gmail.com>
2021-12-07 18:28:36 +0100

    ui: ant draft overview
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/drafts/utils/transformSchema.js
@@ -0,0 +1,27 @@
+import _omit from "lodash/omit";
+
+export const transformSchema = schema => {
+  const schemaFieldsToRemove = [
+    "_access",
+    "_deposit",
+    "_cap_status",
+    "_buckets",
+    "_files",
+    "$ana_type",
+    "$schema",
+    "general_title",
+    "_experiment",
+    "_fetched_from",
+    "_user_edited",
+    "control_number",
+    "_review"
+  ];
+
+  schema.properties = _omit(schema.properties, schemaFieldsToRemove);
+  schema = {
+    type: schema.type,
+    properties: schema.properties,
+    dependencies: schema.dependencies
+  };
+  return schema;
+};
