1db93cc13c8a6a6298cec7bf5267ee32bdc8c0d6
papadopan <antonios.papadopan@gmail.com>
2021-12-07 18:28:36 +0100

    ui: ant draft overview
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/drafts/components/Overview/OverviewLoading.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Skeleton, Space } from "antd";
+
+const OverviewLoading = () => {
+  return (
+    <Space size="middle" direction="vertical" style={{ width: "100%" }}>
+      <Skeleton active />
+      <Skeleton active />
+      <Skeleton active />
+      <Skeleton active />
+      <Skeleton active />
+    </Space>
+  );
+};
+
+export default OverviewLoading;
