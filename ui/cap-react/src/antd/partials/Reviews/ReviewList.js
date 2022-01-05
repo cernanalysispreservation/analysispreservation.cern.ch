e98ac40adf5c37730b788c9c928d572406d85752
papadopan <antonios.papadopan@gmail.com>
2022-01-05 18:38:25 +0200

    ui: ant published preview
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/partials/Reviews/ReviewList.js
@@ -0,0 +1,93 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, List } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+import ShowMoreText from "../ShowMoreText";
+import Description from "./Description";
+
+const ReviewList = ({
+  review,
+  isReviewingPublished,
+  reviewPublished,
+  reviewDraft,
+  draft_id
+}) => {
+  return (
+    <List
+      style={{ maxHeight: "50vh", overflowX: "hidden" }}
+      dataSource={review}
+      renderItem={item => (
+        <List.Item
+          actions={
+            !item.resolved && [
+              <Button
+                key="resolve"
+                type="primary"
+                onClick={() =>
+                  isReviewingPublished
+                    ? reviewPublished(
+                        {
+                          action: "resolve",
+                          id: item.id
+                        },
+                        "resolved"
+                      )
+                    : reviewDraft(
+                        draft_id,
+                        {
+                          action: "resolve",
+                          id: item.id
+                        },
+                        "resolved"
+                      )
+                }
+              >
+                Resolve
+              </Button>,
+              <Button
+                key="delete"
+                danger
+                icon={<DeleteOutlined />}
+                onClick={() =>
+                  isReviewingPublished
+                    ? reviewPublished(
+                        {
+                          id: item.id,
+                          action: "delete"
+                        },
+                        "deleted"
+                      )
+                    : reviewDraft(
+                        draft_id,
+                        {
+                          id: item.id,
+                          action: "delete"
+                        },
+                        "deleted"
+                      )
+                }
+              />
+            ]
+          }
+        >
+          <List.Item.Meta
+            description={
+              <ShowMoreText italic={item.resolved}>{item.body}</ShowMoreText>
+            }
+            title={<Description review={item} />}
+          />
+        </List.Item>
+      )}
+    />
+  );
+};
+
+ReviewList.propTypes = {
+  review: PropTypes.object,
+  isReviewingPublished: PropTypes.bool,
+  reviewPublished: PropTypes.func,
+  reviewDraft: PropTypes.func,
+  draft_id: PropTypes.string
+};
+
+export default ReviewList;
