91c779ae92931b0cb5fd10f9469de3bc2857262c
papadopan <antonios.papadopan@gmail.com>
2022-01-05 18:38:25 +0200

    ui: ant published preview
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/partials/JSONSchemaPreviewer/JSONSchemaPreviewer.js
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Space } from "antd";
+import Form from "react-jsonschema-form";
+
+import FieldTemplate from "../../../components/drafts/form/themes/grommet-preview/templates/FieldTemplate";
+import ArrayFieldTemplate from "../../../components/drafts/form/themes/grommet/templates/ArrayFieldTemplate";
+import ObjectFieldTemplate from "../../../components/drafts/form/themes/grommet/templates/ObjectFieldTemplate";
+
+import widgets from "../../../components/drafts/form/themes/grommet-preview/widgets";
+import fields from "../../../components/drafts/form/themes/grommet-preview/fields";
+
+const JSONSchemaPreviewer = ({
+  isPublished,
+  schema,
+  uiSchema,
+  children,
+  display = "tabView",
+  onChange,
+  onSubmit
+}) => {
+  return (
+    schema && (
+      <Form
+        schema={schema}
+        FieldTemplate={FieldTemplate}
+        ObjectFieldTemplate={ObjectFieldTemplate}
+        ArrayFieldTemplate={ArrayFieldTemplate}
+        showErrorList={false}
+        widgets={widgets}
+        fields={fields}
+        uiSchema={{
+          "ui:readonly": true,
+          ...uiSchema,
+          "ui:object": display
+        }}
+        liveValidate={false}
+        noValidate={true}
+        onError={() => {}}
+        formData={{}}
+        onBlur={() => {}}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        formContext={{
+          tabView: display === "tabView",
+          readonlyPreview: true,
+          isPublished: isPublished
+        }}
+      >
+        {children}
+      </Form>
+    )
+  );
+};
+
+JSONSchemaPreviewer.propTypes = {
+  display: PropTypes.string,
+  children: PropTypes.node,
+  isPublished: PropTypes.bool,
+  schema: PropTypes.object,
+  uiSchema: PropTypes.object,
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func
+};
+
+export default JSONSchemaPreviewer;
