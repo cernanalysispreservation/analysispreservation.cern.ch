cernanalysispreservation/analysis-preservation.cern.ch

ui: immutable for search facets
* closes #2199

Signed-off-by: papadopan <antonios.papadopan@gmail.com>

ui/cap-react/src/components/search/SearchFacet/SubFacet/SubFacet.js
@@ -2,13 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import Box from "grommet/components/Box";
 import CheckBox from "grommet/components/CheckBox";
+import { Map } from "immutable";
 
-const SubFacet = ({ type, field, isAggSelected, selectedAggs, onChange }) => {
+const SubFacet = ({
+  type,
+  field,
+  ff,
+  isAggSelected,
+  selectedAggs,
+  onChange
+}) => {
   if (type === "facet_type_version") {
-    return field[type].buckets.map(nested_field => (
+    return ff.getIn([type, "buckets"]).map(nested_field => (
       <Box
         size="medium"
-        key={String(nested_field.key)}
+        key={String(nested_field.get("key"))}
         direction="row"
         align="start"
         margin={{ left: "medium" }}
@@ -17,17 +25,17 @@ const SubFacet = ({ type, field, isAggSelected, selectedAggs, onChange }) => {
         }}
       >
         <CheckBox
-          label={`${nested_field.key} ${
-            typeof nested_field.doc_count === "object"
-              ? `(${nested_field.doc_count.doc_count})`
-              : `(${nested_field.doc_count})`
+          label={`${nested_field.get("key")} ${
+            Map.isMap(nested_field.get("doc_count"))
+              ? `(${nested_field.getIn(["doc_count", "doc_count"])})`
+              : `(${nested_field.get("doc_count")})`
           }`}
-          key={`${field.key}-${nested_field.key}`}
-          name={`${field.key}-${nested_field.key}`}
+          key={`${ff.get("key")}-${nested_field.get("key")}`}
+          name={`${ff.get("key")}-${nested_field.get("key")}`}
           checked={
             isAggSelected(
               selectedAggs[type.replace("facet_", "")],
-              `${field.key}-${nested_field.key}`
+              `${ff.get("key")}-${nested_field.get("key")}`
             )
               ? true
               : false
@@ -38,10 +46,10 @@ const SubFacet = ({ type, field, isAggSelected, selectedAggs, onChange }) => {
     ));
   }
 
-  return field[type].buckets.map(nested_field => (
+  return ff.getIn([type, "buckets"]).map(nested_field => (
     <Box
       size="medium"
-      key={String(nested_field.key)}
+      key={String(nested_field.get("key"))}
       direction="row"
       align="start"
       margin={{ left: "medium" }}
@@ -50,13 +58,13 @@ const SubFacet = ({ type, field, isAggSelected, selectedAggs, onChange }) => {
       }}
     >
       <CheckBox
-        label={nested_field.key}
-        key={nested_field.key}
-        name={String(nested_field.key)}
+        label={nested_field.get("key")}
+        key={nested_field.get("key")}
+        name={String(nested_field.get("key"))}
         checked={
           isAggSelected(
             selectedAggs[type.replace("facet_", "")],
-            nested_field.key
+            nested_field.get("key")
           )
             ? true
             : false
@@ -64,9 +72,9 @@ const SubFacet = ({ type, field, isAggSelected, selectedAggs, onChange }) => {
         onChange={onChange(type.replace("facet_", ""))}
       />
       <Box align="end">
-        {typeof nested_field.doc_count === "object"
-          ? nested_field.doc_count.doc_count
-          : nested_field.doc_count}
+        {Map.isMap(nested_field.get("doc_count"))
+          ? nested_field.getIn(["doc_count", "doc_count"])
+          : nested_field.get("doc_count")}
       </Box>
     </Box>
   ));

ui/cap-react/src/components/search/SearchFacets.js
@@ -12,6 +12,7 @@ import SearchFacetLoading from "./SearchFacetLoading";
 import CheckBox from "grommet/components/CheckBox";
 
 import SearchFacet from "./SearchFacet";
+import { Map } from "immutable";
 
 // import FiltersPreview from './components/FiltersPreview';
 
@@ -26,24 +27,33 @@ class SearchFacets extends React.Component {
     };
   }
 
-  constructFacets = aggs => {
+  constructFacets = (aggs, regs) => {
     let facets = {};
+    let ff = Map({});
 
     let keys = Object.keys(aggs).filter(key => {
       return typeof aggs[key] === "object";
     });
-    for (let key of keys) {
-      let obj = {};
-      if (key.startsWith("facet_")) {
-        obj[key.replace("facet_", "")] =
-          "filtered" in aggs[key] ? aggs[key]["filtered"] : aggs[key];
-      } else {
-        obj = this.constructFacets(aggs[key]);
-      }
-      Object.assign(facets, obj);
-    }
 
-    return facets;
+    regs &&
+      regs.mapEntries(item => {
+        if (!Map.isMap(item[1])) return;
+        let oo = Map({});
+
+        if (item[0].startsWith("facet_")) {
+          let newItem = item[1].has("filtered")
+            ? item[1].get("filtered")
+            : item[1];
+
+          oo = oo.set(item[0].replace("facet_", ""), newItem);
+        } else {
+          oo = this.constructFacets(item[1].toJS(), item[1]);
+        }
+
+        ff = ff.merge(oo);
+      });
+
+    return ff;
   };
 
   _onChange(category, event) {
@@ -72,7 +82,8 @@ class SearchFacets extends React.Component {
   }
 
   updateHistory(selectedAggs, category) {
-    let facet = this.constructFacets(this.props.aggs);
+    let facet = this.constructFacets(this.props.aggs, this.props.regs);
+
     let catType;
     if (facet[category]) {
       let temp = Object.keys(facet[category].buckets[0]).filter(name =>
@@ -178,12 +189,12 @@ class SearchFacets extends React.Component {
     let facets_result = null;
 
     if (this.props.aggs) {
-      let facets = this.constructFacets(this.props.aggs);
+      let facet = this.constructFacets(this.props.aggs, this.props.regs);
+      let facets = facet.toJS();
 
       if (this.props.removeType) {
         delete facets.type;
       }
-      let categories = Object.keys(facets);
 
       facets_result = (
         <Box
@@ -192,22 +203,21 @@ class SearchFacets extends React.Component {
           margin={{ vertical: "medium" }}
           className="search_facets"
         >
-          {categories.map(category => {
-            return (
-              <Box key={category}>
-                {facets[category].buckets.length > 0 && (
-                  <Box key={category} separator="bottom">
-                    <SearchFacet
-                      category={category}
-                      facets={facets}
-                      isAggSelected={this.isAggSelected}
-                      selectedAggs={this.props.selectedAggs}
-                      onChange={category => this._onChange.bind(this, category)}
-                    />
-                  </Box>
-                )}
-              </Box>
-            );
+          {facet.entrySeq().map(item => {
+            if (item[1].get("buckets").size > 0) {
+              return (
+                <Box key={item[0]} separator="bottom">
+                  <SearchFacet
+                    category={item[0]}
+                    facets={facets}
+                    ffacet={facet}
+                    isAggSelected={this.isAggSelected}
+                    selectedAggs={this.props.selectedAggs}
+                    onChange={category => this._onChange.bind(this, category)}
+                  />
+                </Box>
+              );
+            }
           })}
         </Box>
       );

ui/cap-react/src/components/search/SearchPage.js
@@ -195,11 +195,12 @@ class SearchPage extends React.Component {
               />
             }
           />
-          <Box direction="row" >
+          <Box direction="row">
             <Box id="sidebar">
               <SearchFacets
                 removeType={this.props.match.params.anatype}
                 aggs={_aggs}
+                regs={this.props.results.get("aggregations")}
                 selectedAggs={this.props.selectedAggs}
                 onChange={this._toggleAggs}
               />

cernanalysispreservation/analysispreservation.cern.ch

ui/cap-react/src/components/search/SearchFacet/SubFacet/SubFacet.js
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import Box from "grommet/components/Box";
 import CheckBox from "grommet/components/CheckBox";
+import { Map } from "immutable";
 
 const SubFacet = ({ type, field, isAggSelected, selectedAggs, onChange }) => {
-  return field[type].buckets.map(nested_field => (
+  return field.getIn([type, "buckets"]).map(nested_field => (
     <Box
       size="medium"
-      key={String(nested_field.key)}
+      key={String(nested_field.get("key"))}
       direction="row"
       align="start"
       margin={{ left: "medium" }}
@@ -16,17 +17,17 @@ const SubFacet = ({ type, field, isAggSelected, selectedAggs, onChange }) => {
       }}
     >
       <CheckBox
-        label={`${nested_field.key} ${
-          typeof nested_field.doc_count === "object"
-            ? `(${nested_field.doc_count.doc_count})`
-            : `(${nested_field.doc_count})`
+        label={`${nested_field.get("key")} ${
+          Map.isMap(nested_field.get("doc_count"))
+            ? `(${nested_field.getIn(["doc_count", "doc_count"])})`
+            : `(${nested_field.get("doc_count")})`
         }`}
-        key={nested_field.key}
-        name={String(nested_field.key)}
+        key={nested_field.get("key")}
+        name={String(nested_field.get("key"))}
         checked={
           isAggSelected(
             selectedAggs[type.replace("facet_", "")],
-            nested_field.key
+            nested_field.get("key")
           )
             ? true
             : false

ui/cap-react/src/components/search/SearchFacets.js
@@ -12,6 +12,7 @@ import SearchFacetLoading from "./SearchFacetLoading";
 import CheckBox from "grommet/components/CheckBox";
 
 import SearchFacet from "./SearchFacet";
+import { Map } from "immutable";
 
 // import FiltersPreview from './components/FiltersPreview';
 
@@ -26,21 +27,26 @@ class SearchFacets extends React.Component {
     };
   }
 
-  constructFacets = aggs => {
-    let facets = {};
-    let keys = Object.keys(aggs).filter(key => {
-      return typeof aggs[key] === "object";
-    });
-    for (let key of keys) {
-      let obj = {};
-      if (key.startsWith("facet_")) {
-        obj[key.replace("facet_", "")] =
-          "filtered" in aggs[key] ? aggs[key]["filtered"] : aggs[key];
-      } else {
-        obj = this.constructFacets(aggs[key]);
-      }
-      Object.assign(facets, obj);
-    }
+  constructFacets = (aggs = {}) => {
+    let facets = Map({});
+
+    aggs &&
+      aggs.mapEntries(item => {
+        if (!Map.isMap(item[1])) return;
+        let obj = Map({});
+
+        if (item[0].startsWith("facet_")) {
+          let newItem = item[1].has("filtered")
+            ? item[1].get("filtered")
+            : item[1];
+
+          obj = obj.set(item[0].replace("facet_", ""), newItem);
+        } else {
+          obj = this.constructFacets(item[1]);
+        }
+
+        facets = facets.merge(obj);
+      });
 
     return facets;
   };
@@ -72,6 +78,7 @@ class SearchFacets extends React.Component {
 
   updateHistory(selectedAggs, category) {
     let facet = this.constructFacets(this.props.aggs);
+
     let catType;
     if (facet[category]) {
       let temp = Object.keys(facet[category].buckets[0]).filter(name =>
@@ -177,19 +184,25 @@ class SearchFacets extends React.Component {
     let facets_result = null;
 
     if (this.props.aggs) {
-      let facets = this.constructFacets(this.props.aggs);
+      let facet = this.constructFacets(this.props.aggs);
 
       if (this.props.removeType) {
-        delete facets.type;
+        facet = facet.delete("type");
       }
 
       // Get and sort by order aggregations
       let categories = [];
-      for (let key in facets) {
-        categories.push([key, facets[key].meta && facets[key].meta.order ? facets[key].meta.order : 9999 ]);
-      }
-      categories.sort((a, b) => (a[1] - b[1]) )
-      categories = categories.map(c => c[0]);
+      facet.mapEntries(item => {
+        categories.push([
+          item[0],
+          item[1],
+          item[1].hasIn(["meta", "order"])
+            ? item[1].getIn(["meta", "order"])
+            : 9999
+        ]);
+      });
+      categories.sort((a, b) => a[2] - b[2]);
+      categories = categories.map(c => [c[0], c[1]]);
 
       facets_result = (
         <Box
@@ -198,22 +211,20 @@ class SearchFacets extends React.Component {
           margin={{ vertical: "medium" }}
           className="search_facets"
         >
-          {categories.map(category => {
-            return (
-              <Box key={category}>
-                {facets[category].buckets.length > 0 && (
-                  <Box key={category} separator="bottom">
-                    <SearchFacet
-                      category={category}
-                      facets={facets}
-                      isAggSelected={this.isAggSelected}
-                      selectedAggs={this.props.selectedAggs}
-                      onChange={category => this._onChange.bind(this, category)}
-                    />
-                  </Box>
-                )}
-              </Box>
-            );
+          {categories.map(item => {
+            if (item[1].get("buckets").size > 0) {
+              return (
+                <Box key={item[0]} separator="bottom">
+                  <SearchFacet
+                    category={item[0]}
+                    facet={facet}
+                    isAggSelected={this.isAggSelected}
+                    selectedAggs={this.props.selectedAggs}
+                    onChange={category => this._onChange.bind(this, category)}
+                  />
+                </Box>
+              );
+            }
           })}
         </Box>
       );

ui/cap-react/src/components/search/SearchPage.js
@@ -119,11 +119,9 @@ class SearchPage extends React.Component {
     let queryParams = 0;
 
     let _results = {};
-    let _aggs;
 
     if (this.props.results) {
       _results = this.props.results.toJS();
-      _aggs = _results.aggregations;
     }
 
     // count the number of filters in order to update the button text
@@ -202,7 +200,7 @@ class SearchPage extends React.Component {
             properties={
               <SearchFacets
                 removeType={this.props.match.params.anatype}
-                aggs={_aggs}
+                aggs={this.props.results.get("aggregations")}
                 selectedAggs={this.props.selectedAggs}
                 onChange={this._toggleAggs}
               />
@@ -212,7 +210,7 @@ class SearchPage extends React.Component {
             <Box id="sidebar">
               <SearchFacets
                 removeType={this.props.match.params.anatype}
-                aggs={_aggs}
+                aggs={this.props.results.get("aggregations")}
                 selectedAggs={this.props.selectedAggs}
                 onChange={this._toggleAggs}
               />