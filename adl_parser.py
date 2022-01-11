142614a65d257eaf6b6d5800e7d6e9fafe7de5c0
Parth Shandilya <parth.shandilya@cern.ch>
2022-01-11 15:31:55 +0100

    services: implement adl parser for different block types
    Signed-off-by: Parth Shandilya <parth.shandilya@cern.ch>

--- adl_parser.py
+++ adl_parser.py
@@ -1,72 +1,95 @@
-"""
-json = {
-	"block_<number>": {
-		"blocktype": "blockname",
-		"k1": "v1",
-		"k2": ["v2", "v3"]
-	}
-}
-"""
+block_names = ("object", "region", "info", "table")
+table_k_v = "# val    err-     err+     xmin     xmax"
+
 
 def adl_parser(adl_file='adl.adl'):
-    """Returns the parsed file as JSON object
+    """Returns the ADL parsed file as JSON object
 
     :param type: File
-    :param adl_file: Input .adl from a user.
+    :param adl_file: Input .adl from the user.
     :return : JSON object
     """
-    json_parsed={}
-    indentation = []
-    indentation.append(0)
-    block_number = 1
+    json_parsed = {
+        "object": [],
+        "region": [],
+        "info": [],
+        "table": []
+    }
     block_data = {}
     parsing_block = False
+    parsing_table = False
 
-    f = open("adl.adl", 'r')
+    # Read the ADL file
+    f = open(adl_file, 'r')
 
     for line in f:
-        #import pdb; pdb.set_trace()
-        # Skip comment lines
+        # Skip comment lines outside block
         if line.startswith("#"):
             continue
 
         # Skip empty lines w/o whitespaces or tabs
         if line in ['\n', '\r\n']:
+            # Pop out the previous block when new line appears
             if parsing_block:
-                json_parsed[block_number] = block_data
+                json_parsed[block_type].append(block_data)
                 block_data = {}
-                block_number += 1
                 parsing_block = False
+                parsing_table = False
             else:
                 continue
 
-        content = line.strip()
-        indent = len(line) - len(content)
+        line = line.strip()
+        if not line:
+            continue
 
-        if content.startswith("object") and not block_data:
-            block_name = content.split(" ", 1)[-1]
-            block_data["object"] = block_name
+        # Check for block types and parse the block data
+        if line.startswith(block_names) and not block_data:
+            block = line.split(" ", 1)
+            block_type = block[0]
+            block_data["name"] = block[-1]
             parsing_block = True
             continue
 
-        elif content.startswith("object") and block_data:
-            json_parsed[block_number] = block_data
+        # Pop out the block when new block starts without empty line
+        elif line.startswith(block_names) and block_data:
+            json_parsed[block_type].append(block_data)
             block_data = {}
-            block_name = content.split(" ", 1)[-1]
-            block_data["object"] = block_name
-            block_number += 1
+            block_data["name"] = line.split(" ", 1)[-1]
             parsing_block = True
-            indentation = [0]
+            parsing_table = False
             continue
 
-        if indent > indentation[-1] or parsing_block:
-            indentation.append(indent)
-            data = content.split(" ", 1)
-            if isinstance(block_data.get(data[0]), list):
-                block_data[data[0]].append(data[-1])
-            elif isinstance(block_data.get(data[0]), str):
-                block_data[data[0]] = [block_data.get(data[0]), data[-1]]
-            else:
-                block_data[data[0]] = data[-1]        
+        # Parse the keyword value pair under a block
+        if parsing_block:
+            # Skip comment lines inside the block
+            if line.startswith("#") and line != table_k_v:
+                continue
 
-    print(json_parsed)
+            # Parse the rows for the table block
+            if block_type == "table":
+                if line == table_k_v:
+                    parsing_table = True
+                    block_data["data"] = []
+                    continue
+                if parsing_table:
+                    values = list(filter(None, line.split(" ")))
+                    if len(values) == 5:
+                        block_data["data"].append({
+                            "val": values[0],
+                            "err-": values[1],
+                            "err+": values[2],
+                            "xmin": values[3],
+                            "xmax": values[4],
+                        })
+                    continue
+            keyword_value = line.split(" ", 1)
+            if keyword_value[0] == "select":
+                # use list for storing `select` values
+                if block_data.get(keyword_value[0]):
+                    block_data[keyword_value[0]].append(keyword_value[-1])
+                else:
+                    block_data[keyword_value[0]] = [keyword_value[-1]]
+            else:
+                block_data[keyword_value[0]] = keyword_value[-1]
+
+    return json_parsed
