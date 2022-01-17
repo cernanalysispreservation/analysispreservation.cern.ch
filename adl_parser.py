0cc6070ce76ab662e7193a8f65089eae56749602
Parth Shandilya <parth.shandilya@cern.ch>
2022-01-17 17:26:34 +0100

    services: add requested changes
    Signed-off-by: Parth Shandilya <parth.shandilya@cern.ch>

--- adl_parser.py
+++ adl_parser.py
@@ -1,4 +1,5 @@
-block_names = ("object", "region", "info", "table")
+block_names = ("object", "region", "info", "table ")
+keywords = ("define", "select", "reject", "take", "using", "sort", "weight", "histo")
 table_k_v = "# val    err-     err+     xmin     xmax"
 
 
@@ -9,15 +10,16 @@ def adl_parser(adl_file='adl.adl'):
     :param adl_file: Input .adl from the user.
     :return : JSON object
     """
+    block_data = {}
+    parsing_block = False
+    parsing_table = False
     json_parsed = {
         "object": [],
         "region": [],
         "info": [],
-        "table": []
+        "table": [],
+        "define": [],
     }
-    block_data = {}
-    parsing_block = False
-    parsing_table = False
 
     # Read the ADL file
     f = open(adl_file, 'r')
@@ -59,37 +61,54 @@ def adl_parser(adl_file='adl.adl'):
             parsing_table = False
             continue
 
-        # Parse the keyword value pair under a block
-        if parsing_block:
-            # Skip comment lines inside the block
-            if line.startswith("#") and line != table_k_v:
-                continue
+        elif line.startswith("define"):
+            line_data = line.split("=")
+            variable_name = line_data[0].strip().split(" ")[-1]
+            variable_value = line_data[1].strip()
+            json_parsed["define"].append({variable_name: variable_value})
+            continue
 
-            # Parse the rows for the table block
-            if block_type == "table":
-                if line == table_k_v:
-                    parsing_table = True
-                    block_data["data"] = []
+        try:
+            # Parse the keyword value pair under a block
+            if parsing_block:
+                # Skip comment lines inside the block
+                if line.startswith("#") and line != table_k_v:
                     continue
-                if parsing_table:
-                    values = list(filter(None, line.split(" ")))
-                    if len(values) == 5:
-                        block_data["data"].append({
-                            "val": values[0],
-                            "err-": values[1],
-                            "err+": values[2],
-                            "xmin": values[3],
-                            "xmax": values[4],
-                        })
-                    continue
-            keyword_value = line.split(" ", 1)
-            if keyword_value[0] == "select":
-                # use list for storing `select` values
-                if block_data.get(keyword_value[0]):
-                    block_data[keyword_value[0]].append(keyword_value[-1])
+
+                # Parse the rows for the table block
+                if block_type == "table":
+                    if line == table_k_v:
+                        parsing_table = True
+                        block_data["data"] = []
+                        continue
+                    if parsing_table:
+                        values = list(filter(None, line.split(" ")))
+                        if len(values) == 5:
+                            block_data["data"].append({
+                                "val": values[0],
+                                "err-": values[1],
+                                "err+": values[2],
+                                "xmin": values[3],
+                                "xmax": values[4],
+                            })
+                        continue
+                keyword_value = line.split(" ", 1)
+                if keyword_value[0] in keywords:
+                    if keyword_value[0] == "select":
+                        # use list for storing `select` values
+                        if block_data.get(keyword_value[0]):
+                            block_data[keyword_value[0]].append(keyword_value[-1])
+                        else:
+                            block_data[keyword_value[0]] = [keyword_value[-1]]
+                    else:
+                        block_data[keyword_value[0]] = keyword_value[-1]
+                elif block_type == "region" and len(keyword_value) == 1:
+                    block_data["parent"] = keyword_value[-1]
                 else:
-                    block_data[keyword_value[0]] = [keyword_value[-1]]
-            else:
-                block_data[keyword_value[0]] = keyword_value[-1]
+                    raise ValueError('Parsing of Keyword {} is not supported.'.format(keyword_value[0]))
+        except Exception:
+            block_data = {}
+            parsing_block = False
+            parsing_table = False
 
     return json_parsed
