be732e86086fabd91f35e9b74d87f33cbbc224cf
papadopan <antonios.papadopan@gmail.com>
2022-01-18 13:04:02 +0100

    ui: ant forms init
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/forms/Form.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+// import { withTheme } from "@rjsf/core";
+// import { Theme as AntDTheme } from "@rjsf/antd";
+import { Row, Col } from "antd";
+// import Form from "@rjsf/antd";
+import ObjectFieldTemplate from "../../components/drafts/form/themes/grommet/templates/ObjectFieldTemplate";
+import FieldTemplate from "../../components/drafts/form/themes/grommet/templates/FieldTemplate";
+import "./Form.less";
+import { withTheme } from "@rjsf/core";
+import { Theme as AntDTheme } from "@rjsf/antd";
+
+const RJSFForm = props => {
+  const Form = withTheme(AntDTheme);
+  return (
+    <Row className="__Form__">
+      <Col>
+        <Form
+          schema={props.schema}
+          uiSchema={props.uiSchema}
+          formData={props.formData}
+          ObjectFieldTemplate={ObjectFieldTemplate}
+          FieldTemplate={FieldTemplate}
+          liveValidate
+          showErrorList={false}
+        >
+          <span />
+        </Form>
+      </Col>
+    </Row>
+  );
+};
+
+RJSFForm.propTypes = {};
+
+export default RJSFForm;
