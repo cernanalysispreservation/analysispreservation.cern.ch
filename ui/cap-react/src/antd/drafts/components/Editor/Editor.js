3f3e06178007ad5b68f5d2b51757c3fb020136d2
papadopan <antonios.papadopan@gmail.com>
2022-01-18 13:04:02 +0100

    ui: ant forms init
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/drafts/components/Editor/Editor.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { Button, Col, Row, Space, Typography } from "antd";
+import Error from "../../../partials/Error/";
+import { transformSchema } from "../../utils/transformSchema";
+import { canEdit } from "../../utils/permissions";
+
+import Form from "../../../forms";
+const Editor = ({
+  schemaErrors,
+  schemas = { schema: {}, uiSchema: {} },
+  formData,
+  match,
+  initForm,
+  fetchSchemaByNameVersion,
+  canAdmin,
+  canUpdate
+}) => {
+  useEffect(() => {
+    if (match.params.schema_id) {
+      initForm();
+      fetchSchemaByNameVersion(match.params.schema_id);
+    }
+  }, []);
+  if (!schemas) return null;
+  if (schemaErrors.length > 0) {
+    return <Error error={schemaErrors[0]} />;
+  }
+  let _schema =
+    schemas && schemas.schema ? transformSchema(schemas.schema) : null;
+
+  return (
+    <Col span={24}>
+      {canEdit(canAdmin, canUpdate) && (
+        <Row
+          justify="space-between"
+          style={{
+            padding: "5px 10px",
+            background: "#fff",
+            marginBottom: "10px"
+          }}
+        >
+          <Space>
+            <Typography.Text>Mode:</Typography.Text>
+            <Button>Edit</Button>
+            <Button>View</Button>
+          </Space>
+          <Button>Save</Button>
+        </Row>
+      )}
+      <Form
+        formData={formData || {}}
+        schema={_schema}
+        uiSchema={schemas.uiSchema || {}}
+      />
+    </Col>
+  );
+};
+
+Editor.propTypes = {};
+
+export default Editor;
