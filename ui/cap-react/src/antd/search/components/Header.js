1a25ee787cd5fc378db9d0f02737fd36c8b9a700
Pamfilos Fokianos <pamfilosf@gmail.com>
2022-01-20 14:35:38 +0100

    ui: ant - search updates asc/desc, help, etc
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- ui/cap-react/src/antd/search/components/Header.js
+++ ui/cap-react/src/antd/search/components/Header.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import queryString from "query-string";
-import { Button, Row, Select, Space, Typography } from "antd";
-import { FilterOutlined } from "@ant-design/icons";
+import { Button, Row, Radio, Select, Space, Typography } from "antd";
+import { ArrowDownOutlined, ArrowUpOutlined, FilterOutlined } from "@ant-design/icons";
 
 const SORT_OPTIONS = [
-  { value: "mostrecent", label: "Most Recent" },
+  { value: "mostrecent", label: "Newest First" },
+  { value: "-mostrecent", label: "Oldest First" },
   { value: "bestmatch", label: "Best Match" }
 ];
 
 const getValueFromLocation = (value = "mostrecent") => {
   const choices = {
-    mostrecent: "Most Recent",
+    mostrecent: "Newest First",
+    "-mostrecent": "Oldest First",
     bestmatch: "Best Match"
   };
 
@@ -27,6 +29,23 @@ const Header = ({
 }) => {
   const total = results.getIn(["hits", "total"]) || 0;
 
+  let searchParams = queryString.parse(location.search)
+  let sortParam = searchParams.sort || "mostrecent";
+  let isDescending = sortParam && sortParam[0] == "-";
+  // TODO: For asc/desc sorting
+  // let sortValue = isDescending ? sortParam.substring(1) : sortParam;
+  // let sortDesc = isDescending? "desc" : "asc";
+  // const [descSort, setDescSort] = useState(sortDesc);
+
+  const updateSort = (sort) => {
+    sort = sort.target.value;
+    if (isDescending && sort == "asc"){
+      onChange(sortParam.substring(1))
+    }
+    else if (!isDescending && sort == "desc"){
+      onChange(`-${sortParam}`)
+    }
+  }
   return (
     <Row style={{ marginBottom: "10px" }} justify="space-between">
       {queryString.parse(location.search).q ? (
@@ -47,11 +66,10 @@ const Header = ({
             Filters
           </Button>
         )}
-        {total > 0 && (
+        <Space>
           <Select
-            defaultValue={
-              queryString.parse(location.search).sort || "mostrecent"
-            }
+            size="small"
+            defaultValue={sortParam}
             style={{ width: 120 }}
             onChange={onChange}
           >
@@ -61,7 +79,31 @@ const Header = ({
               </Select.Option>
             ))}
           </Select>
-        )}
+          {/* 
+          <Radio.Group size="small" value={descSort} onChange={updateSort}>
+            <Radio.Button value="asc">
+              <ArrowUpOutlined type="primary" />
+            </Radio.Button>
+            <Radio.Button value="desc">
+              <ArrowDownOutlined />
+            </Radio.Button>
+            <Select
+              size="small"
+              defaultValue={
+                sortValue || "mostrecent"
+              }
+              style={{ width: 120 }}
+              onChange={onChange}
+            >
+              {SORT_OPTIONS.map(item => (
+                <Select.Option value={item.value} key={item.value}>
+                  {getValueFromLocation(item.value)}
+                </Select.Option>
+              ))}
+            </Select>
+          </Radio.Group>
+          */}
+        </Space>
       </Space>
     </Row>
   );
