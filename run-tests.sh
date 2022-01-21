71d3316fe6c3edbb8a5727fb096c1706f4ea2f2c
Parth Shandilya <parth.shandilya@cern.ch>
2022-01-21 11:57:41 +0100

    global: Improve CI backend workflow and update testing dependencies
    Signed-off-by: Parth Shandilya <parth.shandilya@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -1,15 +1,65 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2018 CERN.
+# This file is part of CERN Analysis Preservation Framework.
+# Copyright (C) 2022 CERN.
 #
-# CERN Analysis Preservation is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# CERN Analysis Preservation Framework is free software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# CERN Analysis Preservation Framework is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CERN Analysis Preservation Framework; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
 
-# pydocstyle cap && \
-python setup.py test
+# Quit on errors
+set -o errexit
 
-#pydocstyle cap tests docs && \
-#isort -rc -c -df && \
-# check-manifest --ignore ".travis-*,docs/_build*" && \
-# sphinx-build -qnNW docs docs/_build/html && \
+# Quit on unbound symbols
+set -o nounset
+
+check_script () {
+    shellcheck run-tests.sh
+}
+
+check_flake8 () {
+    flake8 .
+}
+
+check_manifest () {
+    check-manifest
+}
+
+check_pytest () {
+    python setup.py test
+}
+
+
+if [ $# -eq 0 ]; then
+    check_script
+    check_flake8
+    check_manifest
+    check_pytest
+fi
+
+for arg in "$@"
+do
+    case $arg in
+        --check-shellscript) check_script;;
+        --check-flake8) check_flake8;;
+        --check-manifest) check_manifest;;
+        --check-pytest) check_pytest;;
+        *)
+    esac
+done
