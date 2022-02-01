4f0c88e671250b092e54788eb27cb42cdf52cc7b
papadopan <antonios.papadopan@gmail.com>
2022-02-01 16:34:00 +0100

    ui: ant drafts guard
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/partials/RouteGuard/RouteGuard.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Prompt } from "react-router";
+import { Alert, Modal } from "antd";
+
+const RouteGuard = ({ navigate, shouldBlockNavigation }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [lastLocation, setLastLocation] = useState({ pathname: "/" });
+  const [confirmedNavigation, setConfirmedNavigation] = useState(false);
+  const [when, setWhen] = useState(false);
+
+  const ShowModal = location => {
+    setShowModal(true);
+    setLastLocation(location);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleBlockedNavigation = nextLocation => {
+    if (!confirmedNavigation && shouldBlockNavigation(nextLocation)) {
+      ShowModal(nextLocation);
+      return false;
+    }
+    return true;
+  };
+
+  const confirmNavigation = () => {
+    setShowModal(false);
+    setConfirmedNavigation(true);
+  };
+
+  useEffect(() => {
+    setWhen(true);
+  }, []);
+
+  useEffect(
+    () => {
+      if (confirmedNavigation && lastLocation) {
+        navigate(lastLocation.pathname);
+      }
+    },
+    [confirmedNavigation, lastLocation]
+  );
+
+  return (
+    <React.Fragment>
+      <Prompt when={when} message={handleBlockedNavigation} />
+      <Modal
+        visible={showModal}
+        onCancel={closeModal}
+        okButtonProps={{
+          onClick: confirmNavigation
+        }}
+        cancelText="Cancel"
+        okText="Exit without saving"
+        title="Do you want to exit without saving?"
+      >
+        <Alert
+          message="Are you sure you want to leave this page without saving?"
+          description="It seems that you have unsaved updated data in your draft"
+          type="warning"
+          showIcon
+        />
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+RouteGuard.propTypes = {
+  navigate: PropTypes.func,
+  shouldBlockNavigation: PropTypes.func
+};
+
+export default RouteGuard;
