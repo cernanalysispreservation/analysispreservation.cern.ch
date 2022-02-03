9e4ecaa51da37740f999354cb05fbcaeaec2cfe3
papadopan <antonios.papadopan@gmail.com>
2022-02-03 16:01:06 +0100

    ui: ant cypress settings
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/cypress/integration/settings/settings_spec.js
+++ ui/cap-react/cypress/integration/settings/settings_spec.js
@@ -3,39 +3,26 @@ describe("Settings Page", function() {
     "this is my new title" +
     new Date().toLocaleString("en-GB", { timeZone: "UTC" });
   it("add new token", function() {
-    cy.loginUrl("info@inveniosoftware.org", "infoinfo");
+    cy.loginUrl("info@inveniosoftware.org", "infoinfo", "/settings");
 
-    // open menu
-    cy.get("[data-cy=header-menu]").click();
+    // click button to add token
+    cy.get("[data-cy=settingsAddToken]").click({ force: true });
 
-    // select settings
-    cy.get("[data-cy=settings-menuitem]").click({ force: true });
+    // create a new token
+    cy.get("[data-cy=settingsTokenInput] input").type(`${newIdTitle}{enter}`);
 
-    // open the modal
-    cy.get("[data-cy=settings-add-token]").click();
-
-    // type a new id
-    cy.get("[data-cy=settings-token-form] form input").type(
-      `${newIdTitle}{enter}`
-    );
     // check that the new id exists in the list
-    cy.get("[data-cy=settings-table-token-id]").contains(newIdTitle);
+    cy.get("[data-cy=settingsTableTokens]").contains(newIdTitle);
   });
 
   it("Delete the Token from the list", function() {
-    cy.loginUrl("info@inveniosoftware.org", "infoinfo");
-
-    // open menu
-    cy.get("[data-cy=header-menu]").click();
-
-    // select settings
-    cy.get("[data-cy=settings-menuitem]").click({ force: true });
+    cy.loginUrl("info@inveniosoftware.org", "infoinfo", "/settings");
 
     cy.contains(newIdTitle)
       .parent("tr")
       .within(() => {
         cy.get("td")
-          .eq(3)
+          .eq(2)
           .click();
       });
   });
