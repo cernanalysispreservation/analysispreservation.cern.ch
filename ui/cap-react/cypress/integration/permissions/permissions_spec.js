3dbdbeab35f4b22696264b05a36a2cfd3e1d0c93
papadopan <antonios.papadopan@gmail.com>
2022-02-04 17:00:04 +0100

    ui: ant cypress permissions

--- ui/cap-react/cypress/integration/permissions/permissions_spec.js
+++ ui/cap-react/cypress/integration/permissions/permissions_spec.js
@@ -2,123 +2,76 @@ import { CREATE } from "../../routes";
 
 describe("Validate Deposit Groups", () => {
   it("Deposit Groups for a  CMS member", () => {
-    cy.login("cms@inveniosoftware.org", "cmscms");
-
-    cy.visit(CREATE);
+    // login and navigate to create route
+    cy.loginUrl("cms@inveniosoftware.org", "cmscms", CREATE);
 
     // find out if the deposit groups are the correct ones
     // CMS Analysis and CMS Statistics Questionnaire
-    cy.get("[data-cy=deposit-group-list] > div")
-      .children()
+    cy.get("[data-cy=anatype]")
+      .children("label")
       .each(value => {
         cy.wrap(value)
-          .get("[data-cy=deposit-group-name]")
-          .should(
-            "have.attr",
-            "name",
-            "CMS Analysis" || "CMS Statistics Questionnaire"
-          )
-          .should(
-            "not.have.attr",
-            "name",
-            "ATLAS Analysis" || "ALICE Analysis" || "LHCb Analysis"
-          );
+          .get("span")
+          .contains("CMS Analysis" || "CMS Statistics Questionnaire");
       });
   });
   it("Deposit Groups for an ATLAS member", () => {
-    cy.login("atlas@inveniosoftware.org", "atlasatlas");
-
-    cy.visit(CREATE);
+    cy.loginUrl("atlas@inveniosoftware.org", "atlasatlas", CREATE);
 
     // find out if the deposit groups are the correct ones
-    // CMS Analysis and CMS Statistics Questionnaire
-    cy.get("[data-cy=deposit-group-list] > div")
-      .children()
+    // Atlas Analysis
+    cy.get("[data-cy=anatype]")
+      .children("label")
       .each(value => {
         cy.wrap(value)
-          .get("[data-cy=deposit-group-name]")
-          .should("have.attr", "name", "ATLAS Analysis");
-      })
-      .should(
-        "not.have.attr",
-        "name",
-        "CMS Analysis" ||
-          "CMS Statistics Questionnaire" ||
-          "ALICE Analysis" ||
-          "LHCb Analysis"
-      );
+          .get("span")
+          .contains("ATLAS Analysis");
+      });
   });
   it("Deposit Groups for an Alice member", () => {
-    cy.login("alice@inveniosoftware.org", "alicealice");
-
-    cy.visit(CREATE);
+    cy.loginUrl("alice@inveniosoftware.org", "alicealice", CREATE);
 
     // find out if the deposit groups are the correct ones
-    // CMS Analysis and CMS Statistics Questionnaire
-    cy.get("[data-cy=deposit-group-list] > div")
-      .children()
+    // Alice Analysis
+    cy.get("[data-cy=anatype]")
+      .children("label")
       .each(value => {
         cy.wrap(value)
-          .get("[data-cy=deposit-group-name]")
-          .should("have.attr", "name", "ALICE Analysis")
-          .should(
-            "not.have.attr",
-            "name",
-            "CMS Analysis" ||
-              "CMS Statistics Questionnaire" ||
-              "ATLAS Analysis" ||
-              "LHCb Analysis"
-          );
+          .get("span")
+          .contains("ALICE Analysis");
       });
   });
   it("Deposit Groups for an LHCB member", () => {
-    cy.login("lhcb@inveniosoftware.org", "lhcblhcb");
-
-    cy.visit(CREATE);
+    cy.loginUrl("lhcb@inveniosoftware.org", "lhcblhcb", CREATE);
 
     // find out if the deposit groups are the correct ones
-    // CMS Analysis and CMS Statistics Questionnaire
-    cy.get("[data-cy=deposit-group-list] > div")
-      .children()
+    // LHCb analysis
+    cy.get("[data-cy=anatype]")
+      .children("label")
       .each(value => {
         cy.wrap(value)
-          .get("[data-cy=deposit-group-name]")
-          .should("have.attr", "name", "LHCb Analysis")
-          .should(
-            "not.have.attr",
-            "name",
-            "CMS Analysis" ||
-              "CMS Statistics Questionnaire" ||
-              "ALICE Analysis" ||
-              "ATLAS Analysis"
-          );
+          .get("span")
+          .contains("LHCb Analysis");
       });
   });
 });
 
 describe("Visit create path, with a specific analysis name", () => {
   it("Cms analysis should be  pre selected", () => {
-    cy.login("cms@inveniosoftware.org", "cmscms");
+    cy.loginUrl("cms@inveniosoftware.org", "cmscms", `${CREATE}/cms-analysis`);
 
-    cy.visit(`${CREATE}/cms-analysis`);
-
-    // find out if the deposit groups are the correct ones
-    // CMS Analysis and CMS Statistics Questionnaire
-    cy.get("[data-cy=deposit-group-list] > div")
-      .children()
-      .each(value => {
-        cy.wrap(value)
-          .get("[data-cy=deposit-group-cms-analysis-wrapper]")
-          .get("[data-cy=deposit-group-checkmark]")
-          .should("be.visible");
-      });
+    cy.get("[data-cy=anatype] label.ant-radio-button-wrapper-checked").contains(
+      "CMS Analysis"
+    );
   });
 
   it("Fetch an analysis with no permissions", () => {
-    cy.login("cms@inveniosoftware.org", "cmscms");
+    cy.loginUrl(
+      "cms@inveniosoftware.org",
+      "cmscms",
+      `${CREATE}/atlas-analysis`
+    );
 
-    cy.visit(`${CREATE}/atlas-analysis`);
-
-    cy.get("[data-cy=create-form-error-page]").should("be.visible");
+    cy.get("[data-cy=permission-warning-alert]");
   });
 });
