6e3d0fccaa552f56eeca2039d6dada00720e9521
papadopan <antonios.papadopan@gmail.com>
2022-02-14 14:42:55 +0100

    ui: collection ant
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/antd/dashboard/utils/index.js
+++ ui/cap-react/src/antd/dashboard/utils/index.js
@@ -28,3 +28,27 @@ export const _getList = results => {
     }
   };
 };
+export const _getCollectionList = results => {
+  return {
+    drafts: {
+      all: {
+        list: results.drafts.data,
+        more: results.drafts.more
+      },
+      yours: {
+        list: results.user_drafts.data,
+        more: results.user_drafts.more
+      }
+    },
+    published: {
+      all: {
+        list: results.published.data,
+        more: results.published.more
+      },
+      yours: {
+        list: results.user_published.data,
+        more: results.user_published.more
+      }
+    }
+  };
+};
