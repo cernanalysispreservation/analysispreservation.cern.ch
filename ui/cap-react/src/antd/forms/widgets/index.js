3ada65fd03fded00e64871d6f56a9b7d399cbd6c
papadopan <antonios.papadopan@gmail.com>
2022-03-17 11:28:33 +0100

    ui: ant forms switch widget
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/antd/forms/widgets/index.js
+++ ui/cap-react/src/antd/forms/widgets/index.js
@@ -1,12 +1,14 @@
 import TextWidget from "./TextWidget";
 import SelectWidget from "./SelectWidget";
 import RichEditor from "./RichEditor";
+import SwitchWidget from "./SwitchWidget";
 
 const widgets = {
   text: TextWidget,
   latex: RichEditor,
   select: SelectWidget,
   richeditor: RichEditor,
+  switch: SwitchWidget,
 };
 
 export default widgets;
