f989f8a99c1faccd884c0b278e1570f52ff44c71
papadopan <antonios.papadopan@gmail.com>
2022-03-17 17:07:54 +0100

    ui: ant forms services fields
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/antd/forms/fields/index.js
+++ ui/cap-react/src/antd/forms/fields/index.js
@@ -1,9 +1,12 @@
 import CernUsers from "./cernUsers";
+import ServiceGetter from "./ServiceGetter";
 import TitleField from "./TitleField";
 import CapFiles from "./CapFiles";
+
 const fields = {
   cernUsers: CernUsers,
   CapFiles: CapFiles,
+  idFetcher: ServiceGetter,
   TitleField
 };
 
