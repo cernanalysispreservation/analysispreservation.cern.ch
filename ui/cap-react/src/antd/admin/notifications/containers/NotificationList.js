1d87ea6af8b4de5b170b4d6df402a31e04c4ed08
papadopan <antonios.papadopan@gmail.com>
2022-03-18 16:47:31 +0100

    ui: ant admin notifications
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/admin/notifications/containers/NotificationList.js
@@ -0,0 +1,22 @@
+import { connect } from "react-redux";
+import { createNewNotification } from "../../../../actions/schemaWizard";
+import NotificationList from "../components/NotificationList";
+
+const mapStateToProps = state => ({
+  schemaConfig: state.schemaWizard.getIn([
+    "config",
+    "config",
+    "notifications",
+    "actions"
+  ]),
+  pathname: state.router.location.pathname
+});
+
+const mapDispatchToProps = {
+  createNewNotification: category => createNewNotification(category)
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NotificationList);
---
+++ ui/cap-react/src/antd/admin/notifications/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import {
+  CMS_NOTIFICATION,
+  CMS_NOTIFICATION_CATEGORY,
+  CMS_NOTIFICATION_EDIT
+} from "../../routes";
+import Notifications from "./containers/Notifications";
+import NotificationEdit from "./containers/NotificationEdit";
+
+const index = () => {
+  return (
+    <Switch>
+      <Route exact path={CMS_NOTIFICATION} component={Notifications} />
+      <Route exact path={CMS_NOTIFICATION_CATEGORY} component={Notifications} />
+      <Route path={CMS_NOTIFICATION_EDIT} component={NotificationEdit} />
+    </Switch>
+  );
+};
+
+export default index;
