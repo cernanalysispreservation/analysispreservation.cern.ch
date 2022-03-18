112fc29153a157446c17e6c60198926e8a62e808
papadopan <antonios.papadopan@gmail.com>
2022-03-18 16:47:31 +0100

    ui: ant admin notifications
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/admin/notifications/containers/NotificationEdit.js
@@ -0,0 +1,29 @@
+import { connect } from "react-redux";
+import {
+  createNewNotification,
+  removeNotification,
+  updateNotificationData
+} from "../../../../actions/schemaWizard";
+import NotificationEdit from "../components/NotificationEdit";
+
+const mapStateToProps = state => ({
+  schemaConfig: state.schemaWizard.getIn([
+    "config",
+    "config",
+    "notifications",
+    "actions"
+  ]),
+  pathname: state.router.location.pathname
+});
+
+const mapDispatchToProps = {
+  createNewNotification: category => createNewNotification(category),
+  removeNotification: (index, category) => removeNotification(index, category),
+  updateNotificationData: (data, index, category) =>
+    updateNotificationData(data, index, category)
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NotificationEdit);
