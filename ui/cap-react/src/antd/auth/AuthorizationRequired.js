3e244d4cea061148c2498559625e609563d08b5c
papadopan <antonios.papadopan@gmail.com>
2022-04-11 17:34:50 +0200

    ui: ant resolve legacy imports
    * closes #2538
    * closes #2522
    
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/auth/AuthorizationRequired.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Redirect } from "react-router-dom";
+
+export default ComposedComponent => {
+  class Authentication extends Component {
+    render() {
+      if (!this.props.isLoggedIn) {
+        return (
+          <Redirect
+            to={{
+              pathname: `/login`,
+              search: `?next=${this.props.history.location.pathname}`,
+              state: { next: this.props.history.location.pathname },
+              from: this.props.match.path
+            }}
+          />
+        );
+      }
+
+      return <ComposedComponent {...this.props} />;
+    }
+  }
+
+  Authentication.propTypes = {
+    isLoggedIn: PropTypes.bool,
+    history: PropTypes.object,
+    match: PropTypes.object
+  };
+
+  function mapStateToProps(state) {
+    return { isLoggedIn: state.auth.get("isLoggedIn") };
+  }
+
+  return connect(mapStateToProps)(Authentication);
+};
---
+++ ui/cap-react/src/antd/auth/NoAuthorizationRequired.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+export default function(ComposedComponent) {
+  class NotAuthentication extends Component {
+    constructor(props) {
+      super(props);
+      if (props.isLoggedIn) {
+        props.history.push("/");
+      }
+    }
+
+    shouldComponentUpdate(nextProps) {
+      if (this.props.isLoggedIn === nextProps.isLoggedIn) return false;
+
+      return true;
+    }
+
+    componentWillUpdate(nextProps) {
+      if (nextProps.isLoggedIn) {
+        this.props.history.push("/");
+      }
+    }
+
+    PropTypes = {
+      router: PropTypes.object
+    };
+
+    render() {
+      return <ComposedComponent {...this.props} />;
+    }
+  }
+
+  NotAuthentication.propTypes = {
+    isLoggedIn: PropTypes.bool,
+    history: PropTypes.object
+  };
+
+  function mapStateToProps(state) {
+    return { isLoggedIn: state.auth.get("isLoggedIn") };
+  }
+
+  return connect(mapStateToProps)(NotAuthentication);
+}
