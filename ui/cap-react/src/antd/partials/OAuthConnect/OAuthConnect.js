7182cfa32ea3737dd352367eb524e2bd429d0e80
papadopan <antonios.papadopan@gmail.com>
2022-04-11 17:34:50 +0200

    ui: ant resolve legacy imports
    * closes #2538
    * closes #2522
    
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/partials/OAuthConnect/OAuthConnect.js
@@ -0,0 +1,25 @@
+import React from "react";
+export default class OAuthConnect extends React.PureComponent {
+  componentDidMount() {
+    setTimeout(() => {
+      if (window.opener) {
+        window.opener.focus();
+
+        if (window.opener.loginCallBack) {
+          window.opener.loginCallBack();
+        }
+      }
+      window.close();
+    }, 3000);
+  }
+
+  componentWillUnmount() {
+    if (this.externalWindow) {
+      this.externalWindow.close();
+    }
+  }
+
+  render() {
+    return <div>Connected or not window is closing</div>;
+  }
+}
---
+++ ui/cap-react/src/antd/partials/OAuthPopUp/OAuthPopUp.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class OauthPopup extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    window["loginCallBack"] = function() {
+      if (props.loginCallBack) props.loginCallBack(urlParams.get("next"));
+      return null;
+    };
+  }
+  createPopup = () => {
+    const { url, title = "", width = 500, height = 500 } = this.props;
+    const left = window.screenX + (window.outerWidth - width) / 2;
+    const top = window.screenY + (window.outerHeight - height) / 2.5;
+    this.externalWindow = window.open(
+      url,
+      title,
+      `width=${width},height=${height},left=${left},top=${top}`
+    );
+  };
+
+  render() {
+    return (
+      <div onClick={this.createPopup} style={{ textAlign: "center" }}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+OauthPopup.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.element,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  url: PropTypes.string,
+  loginCallBack: PropTypes.func
+};
