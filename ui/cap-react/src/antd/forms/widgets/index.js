6cd7731d639e4571053dc8c18f3a103bbf91b462
pamfilos <pamfilosf@gmail.com>
2022-06-30 11:36:31 +0300

    ui: ant forms rich editor widget
    Signed-off-by: pamfilos <pamfilosf@gmail.com>
    Co-authored-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/antd/forms/widgets/index.js
+++ ui/cap-react/src/antd/forms/widgets/index.js
@@ -1,9 +1,11 @@
 import TextWidget from "./TextWidget";
 import SelectWidget from "./SelectWidget";
+import RichEditor from "./RichEditor";
 
 const widgets = {
   text: TextWidget,
-  select: SelectWidget
+  select: SelectWidget,
+  richeditor: RichEditor
 };
 
 export default widgets;
