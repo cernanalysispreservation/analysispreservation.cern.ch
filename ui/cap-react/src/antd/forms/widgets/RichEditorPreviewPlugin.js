2692d634cba258a2757a13a26d35b26f2bd1c6d9
pamfilos <pamfilosf@gmail.com>
2022-06-30 11:36:31 +0300

    ui: ant forms rich editor widget
    Signed-off-by: pamfilos <pamfilosf@gmail.com>
    Co-authored-by: papadopan <antonios.papadopan@gmail.com>

---
+++ ui/cap-react/src/antd/forms/widgets/RichEditorPreviewPlugin.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { PluginComponent } from "react-markdown-editor-lite";
+import { EditOutlined, EyeOutlined } from "@ant-design/icons";
+
+export default class RichEditorPreviewPlugin extends PluginComponent {
+  static pluginName = "cap-toggle";
+  static align = "right";
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      isEditView: this.getConfig("isEditView")
+    };
+  }
+
+  handleClick() {
+    if (this.state.isEditView.md) {
+      this.editor.setView({ html: true, md: false });
+      this.setState({ isEditView: { html: true, md: false } });
+    } else {
+      this.editor.setView({ html: false, md: true });
+      this.setState({ isEditView: { html: false, md: true } });
+    }
+  }
+
+  render() {
+    return this.state.isEditView.md ? (
+      <EditOutlined onClick={this.handleClick} />
+    ) : (
+      <EyeOutlined onClick={this.handleClick} />
+    );
+  }
+}
