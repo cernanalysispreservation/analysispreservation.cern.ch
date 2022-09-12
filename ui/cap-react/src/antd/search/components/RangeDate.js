3c156538a07c6eaa808a35117d3e9a7bcd4b7b58
pamfilos <pamfilosf@gmail.com>
2022-09-12 15:36:26 +0200

    ui: ant - adds date range field on facets
    Signed-off-by: pamfilos <pamfilosf@gmail.com>

---
+++ ui/cap-react/src/antd/search/components/RangeDate.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { DatePicker } from "antd";
+import queryString from "query-string";
+import { withRouter } from "react-router-dom";
+const {RangePicker} = DatePicker
+
+const RangeDate = ({ category, history }) => {
+
+  const onChange = (dateRange) => {
+    let params = queryString.parse(history.location.search);
+
+    const location = {
+      search: `${queryString.stringify(
+        Object.assign(params, {
+          [category]: `${dateRange[0]}--${dateRange[1]}`
+        })
+      )}`
+    };
+
+    history.push(location);
+  };
+
+  return (
+    <RangePicker 
+        onChange={(_d, ds) => ds ? onChange(ds) : null}
+    />
+  );
+};
+
+RangeDate.propTypes = {
+  items: PropTypes.object,
+  category: PropTypes.string,
+  history: PropTypes.object
+};
+
+export default withRouter(RangeDate);
