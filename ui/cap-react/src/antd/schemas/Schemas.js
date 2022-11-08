80e29e12efa118ddaa796ffe57a06636afa71937
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2022-11-08 15:44:58 +0100

    ui: add error screen to schemas page

--- ui/cap-react/src/antd/schemas/Schemas.js
+++ ui/cap-react/src/antd/schemas/Schemas.js
@@ -19,6 +19,7 @@ import {
   UndoOutlined,
 } from "@ant-design/icons";
 import JSONInput from "react-json-editor-ajrm";
+import ErrorScreen from "../partials/Error";
 
 const Schemas = ({ match }) => {
   const EDITABLE_FIELDS = [
@@ -42,6 +43,7 @@ const Schemas = ({ match }) => {
   const [options, setOptions] = useState();
   const [showModal, setShowModal] = useState();
   const [jsonError, setJsonError] = useState();
+  const [error, setError] = useState();
 
   useEffect(() => {
     axios
@@ -61,6 +63,9 @@ const Schemas = ({ match }) => {
           }));
         opts.unshift({ label: FULL_SCHEMA, value: "all" });
         setOptions(opts);
+      })
+      .catch(() => {
+        setError(true);
       });
   }, []);
 
@@ -120,133 +125,137 @@ const Schemas = ({ match }) => {
 
   return (
     <DocumentTitle title="Schemas">
-      <React.Fragment>
-        <Modal
-          visible={showModal}
-          onCancel={() => setShowModal(false)}
-          title={`${selection} diff`}
-          width={800}
-          footer={null}
-          bodyStyle={{
-            overflowX: "scroll",
-            overflowY: "auto",
-            maxHeight: "calc(100vh - 200px)",
-          }}
-        >
-          <JsonDiff
-            left={
-              selection === FULL_SCHEMA
-                ? originalSchema
-                : originalSchema[selection]
-            }
-            right={field}
-          />
-        </Modal>
-        <Row justify="center">
-          <Col xs={22} lg={14}>
-            <Row
-              justify="space-between"
-              align="middle"
-              style={{ marginBottom: "1em" }}
-              gutter={10}
-            >
-              <Col flex="auto">
-                <Typography.Title style={{ marginBottom: "0" }}>
-                  {schema_name} {schema_version}
-                </Typography.Title>
-              </Col>
-              <Col>
-                <Tooltip
-                  placement="bottom"
-                  title="Select a field to edit it (if it is editable). 
+      {error ? (
+        <ErrorScreen />
+      ) : (
+        <React.Fragment>
+          <Modal
+            visible={showModal}
+            onCancel={() => setShowModal(false)}
+            title={`${selection} diff`}
+            width={800}
+            footer={null}
+            bodyStyle={{
+              overflowX: "scroll",
+              overflowY: "auto",
+              maxHeight: "calc(100vh - 200px)",
+            }}
+          >
+            <JsonDiff
+              left={
+                selection === FULL_SCHEMA
+                  ? originalSchema
+                  : originalSchema[selection]
+              }
+              right={field}
+            />
+          </Modal>
+          <Row justify="center">
+            <Col xs={22} lg={14}>
+              <Row
+                justify="space-between"
+                align="middle"
+                style={{ marginBottom: "1em" }}
+                gutter={10}
+              >
+                <Col flex="auto">
+                  <Typography.Title style={{ marginBottom: "0" }}>
+                    {schema_name} {schema_version}
+                  </Typography.Title>
+                </Col>
+                <Col>
+                  <Tooltip
+                    placement="bottom"
+                    title="Select a field to edit it (if it is editable). 
                   Check the diff, revert or save the changes to that field, 
                   or go back to 'Full schema' to check the full diff, 
                   revert or save all changes at once."
-                >
-                  <InfoCircleOutlined />
-                </Tooltip>
-              </Col>
-              <Col xs={15} md={6}>
-                {schema && (
-                  <Select
-                    style={{
-                      width: "100%",
-                    }}
-                    options={options}
-                    onChange={handleSelectChange}
-                    defaultValue="all"
-                  />
-                )}
-              </Col>
-              <Col>
-                <Tooltip
-                  placement="bottom"
-                  title={
-                    jsonError
-                      ? "Please fix the syntax errors to be able to view the diff"
-                      : selection === FULL_SCHEMA
-                        ? "View full diff"
-                        : "View field diff"
-                  }
-                >
-                  <Button
-                    icon={<DiffOutlined />}
-                    onClick={() => setShowModal(true)}
-                    disabled={jsonError}
-                  />
-                </Tooltip>
-              </Col>
-              <Col>
-                <Tooltip
-                  placement="bottom"
-                  title={
-                    selection === FULL_SCHEMA
-                      ? "Revert all unsaved changes"
-                      : "Revert unsaved changes to this field"
-                  }
-                >
-                  <Button icon={<UndoOutlined />} onClick={handleRevert} />
-                </Tooltip>
-              </Col>
-              <Col>
-                <Tooltip
-                  placement="bottom"
-                  title={
-                    selection === FULL_SCHEMA
-                      ? "Save all changes"
-                      : !EDITABLE_FIELDS.includes(selection)
-                        ? "Please select an editable field to be able to edit and save the changes"
-                        : "Save changes to this field"
-                  }
-                >
-                  <Button
-                    icon={<SaveOutlined />}
-                    type="primary"
-                    onClick={handleSave}
-                    disabled={
-                      !EDITABLE_FIELDS.includes(selection) &&
-                      selection != FULL_SCHEMA
+                  >
+                    <InfoCircleOutlined />
+                  </Tooltip>
+                </Col>
+                <Col xs={15} md={6}>
+                  {schema && (
+                    <Select
+                      style={{
+                        width: "100%",
+                      }}
+                      options={options}
+                      onChange={handleSelectChange}
+                      defaultValue="all"
+                    />
+                  )}
+                </Col>
+                <Col>
+                  <Tooltip
+                    placement="bottom"
+                    title={
+                      jsonError
+                        ? "Please fix the syntax errors to be able to view the diff"
+                        : selection === FULL_SCHEMA
+                          ? "View full diff"
+                          : "View field diff"
                     }
+                  >
+                    <Button
+                      icon={<DiffOutlined />}
+                      onClick={() => setShowModal(true)}
+                      disabled={jsonError}
+                    />
+                  </Tooltip>
+                </Col>
+                <Col>
+                  <Tooltip
+                    placement="bottom"
+                    title={
+                      selection === FULL_SCHEMA
+                        ? "Revert all unsaved changes"
+                        : "Revert unsaved changes to this field"
+                    }
+                  >
+                    <Button icon={<UndoOutlined />} onClick={handleRevert} />
+                  </Tooltip>
+                </Col>
+                <Col>
+                  <Tooltip
+                    placement="bottom"
+                    title={
+                      selection === FULL_SCHEMA
+                        ? "Save all changes"
+                        : !EDITABLE_FIELDS.includes(selection)
+                          ? "Please select an editable field to be able to edit and save the changes"
+                          : "Save changes to this field"
+                    }
+                  >
+                    <Button
+                      icon={<SaveOutlined />}
+                      type="primary"
+                      onClick={handleSave}
+                      disabled={
+                        !EDITABLE_FIELDS.includes(selection) &&
+                        selection != FULL_SCHEMA
+                      }
+                    />
+                  </Tooltip>
+                </Col>
+              </Row>
+              <Row justify="center">
+                <Col xs={24}>
+                  <JSONInput
+                    width="100%"
+                    height="calc(100vh - 19em)"
+                    placeholder={field}
+                    onChange={handleEdit}
+                    viewOnly={!EDITABLE_FIELDS.includes(selection)}
+                    theme="light_mitsuketa_tribute"
+                    onKeyPressUpdate={false}
                   />
-                </Tooltip>
-              </Col>
-            </Row>
-            <Row justify="center">
-              <Col xs={24}>
-                <JSONInput
-                  width="100%"
-                  height="calc(100vh - 19em)"
-                  placeholder={field}
-                  onChange={handleEdit}
-                  viewOnly={!EDITABLE_FIELDS.includes(selection)}
-                  theme="light_mitsuketa_tribute"
-                  onKeyPressUpdate={false}
-                />
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </React.Fragment>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </React.Fragment>
+      )}
     </DocumentTitle>
   );
 };
