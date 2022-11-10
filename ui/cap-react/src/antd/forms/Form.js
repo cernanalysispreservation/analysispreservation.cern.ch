66ce0995489878ab875cf35ce02ca00f734002d6
pamfilos <pamfilosf@gmail.com>
2022-11-10 17:00:14 +0100

    ui: form - removes permission field part
    Signed-off-by: pamfilos <pamfilosf@gmail.com>

--- ui/cap-react/src/antd/forms/Form.js
+++ ui/cap-react/src/antd/forms/Form.js
@@ -6,7 +6,6 @@ import FieldTemplate from "./templates/Field/FieldTemplate";
 import fields from "./fields";
 import CAPWidgets from "./widgets";
 
-import _debounce from "lodash/debounce";
 import objectPath from "object-path";
 
 import "./Form.less";
@@ -24,7 +23,6 @@ const RJSFForm = ({
   readonly,
   draftEditor,
   className = [],
-  currentUser,
   ObjectFieldTemplate: Objects,
   ArrayFieldTemplate: Arrays,
   FieldTemplate: Fields,
@@ -33,9 +31,6 @@ const RJSFForm = ({
   liveValidate = false,
   showErrorList = false,
 }) => {
-  let userEmail = currentUser
-    ? currentUser.getIn(["profile", "email"])
-    : "noUserEmail";
   // mainly this is used for the drafts forms
   // we want to allow forms to be saved even without required fields
   // if these fields are not filled in when publishing then an error will be shown
@@ -56,67 +51,6 @@ const RJSFForm = ({
 
     return errors;
   };
-
-  const NESTED_CASES = ["object", "array"];
-  // checks for the user permissions
-  // if the user is in the list then we can allow edit the field
-  // if not in the list then we should make the field readOnly
-
-  const canUserEditField = (obj, user) => {
-    // get the list with the users
-    const users = obj["x-cap-permission"]["users"];
-
-    // if the user is in the list, it means he can edit
-    if (users) {
-      return users.map((i) => i.toLowerCase()).includes(user.toLowerCase());
-    }
-    return false;
-  };
-  const checkSchemaForPermissions = (schema) => {
-    let nextSteps = NESTED_CASES.includes(schema.type)
-      ? schema.type == "object"
-        ? "properties"
-        : "items"
-      : null;
-
-    // check if this block has this field for permission checks
-    schema["x-cap-permission"] &&
-      !canUserEditField(schema, userEmail) &&
-      (schema["readOnly"] = true);
-
-    // if the current block is either an object or an array
-    // then we should examine the nested fields
-    if (nextSteps) {
-      if (schema[nextSteps] && schema[nextSteps]["type"]) {
-        let next = NESTED_CASES.includes(schema[nextSteps].type)
-          ? schema[nextSteps].type == "object"
-            ? "properties"
-            : "items"
-          : null;
-
-        if (next) checkSchemaForPermissions(schema[nextSteps]);
-        else {
-          schema[nextSteps]["x-cap-permission"] &&
-            !canUserEditField(schema[nextSteps], userEmail) &&
-            (schema[nextSteps]["readOnly"] = true);
-        }
-      } else {
-        // in case that we enter in a block that does not have any of the type
-        // then we shoyld start iterating for nested cases
-        schema[nextSteps] &&
-          Object.values(schema[nextSteps]).map((val) => {
-            schema["x-cap-permission"] &&
-              !canUserEditField(schema, userEmail) &&
-              (schema["readOnly"] = true);
-
-            checkSchemaForPermissions(val);
-          });
-      }
-    }
-  };
-
-  // call to update the schema fields
-  checkSchemaForPermissions(schema);
 
   return (
     <Form
