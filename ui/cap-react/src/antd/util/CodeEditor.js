16fdc7cf87ec9df96e436ee1d86314c89382396e
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2022-11-17 12:47:20 +0100

    global: changed flake8 repo to github
    Signed-off-by: Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>

--- ui/cap-react/src/antd/util/CodeEditor.js
+++ ui/cap-react/src/antd/util/CodeEditor.js
@@ -3,37 +3,34 @@ import { basicSetup } from "codemirror";
 import { EditorState } from "@codemirror/state";
 import { EditorView, keymap } from "@codemirror/view";
 import { json, jsonParseLinter } from "@codemirror/lang-json";
-import { indentWithTab } from "@codemirror/commands";
 import { linter, lintGutter } from "@codemirror/lint";
+import { indentWithTab } from "@codemirror/commands";
 
 const CodeEditor = ({ value, isReadOnly, handleEdit, schema }) => {
   const element = useRef(null);
 
-  useEffect(
-    () => {
-      element.current.innerHTML = "";
-      new EditorView({
-        state: EditorState.create({
-          doc: value,
-          extensions: [
-            basicSetup,
-            keymap.of([indentWithTab]),
-            json(),
-            linter(jsonParseLinter()),
-            lintGutter(),
-            EditorState.readOnly.of(isReadOnly),
-            EditorView.updateListener.of(update => {
-              if (update.docChanged) {
-                handleEdit(update.state.doc.toString());
-              }
-            }),
-          ],
-        }),
-        parent: element.current,
-      });
-    },
-    [value, schema]
-  );
+  useEffect(() => {
+    element.current.innerHTML = "";
+    new EditorView({
+      state: EditorState.create({
+        doc: value,
+        extensions: [
+          basicSetup,
+          keymap.of([indentWithTab]),
+          json(),
+          linter(jsonParseLinter()),
+          lintGutter(),
+          EditorState.readOnly.of(isReadOnly),
+          EditorView.updateListener.of((update) => {
+            if (update.docChanged) {
+              handleEdit(update.state.doc.toString());
+            }
+          }),
+        ],
+      }),
+      parent: element.current,
+    });
+  }, [value, schema]);
 
   return <div ref={element} />;
 };
