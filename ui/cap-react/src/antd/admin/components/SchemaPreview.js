89c508d1912474a8fa59d04efd09564dee324a33
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2023-01-16 16:44:32 +0100

    ui: admin layout changes
    Signed-off-by: Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>

--- ui/cap-react/src/antd/admin/components/SchemaPreview.js
+++ ui/cap-react/src/antd/admin/components/SchemaPreview.js
@@ -1,20 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Col, Divider, Space, Typography } from "antd";
+import { Button, Col, Row, Typography } from "antd";
 import SchemaTree from "../containers/SchemaTree";
 import { SettingOutlined } from "@ant-design/icons";
 
 const SchemaPreview = ({ schema, selectProperty }) => {
   return (
-    <Col span={24} style={{ height: "90%" }}>
-      <Divider orientation="left">
-        <Space onClick={() => selectProperty({ schema: [], uiSchema: [] })}>
-          <SettingOutlined />
-          <Typography.Title style={{ margin: 0 }} level={5}>
-            {(schema && schema.get("title")) || "Root"}
-          </Typography.Title>
-        </Space>
-      </Divider>
+    <Col span={24} style={{ height: "90%", marginTop: "16px" }}>
+      <Row
+        justify="space-between"
+        style={{ paddingLeft: "10px", paddingRight: "10px" }}
+      >
+        <Typography.Title level={5}>
+          {(schema && schema.get("title")) || "Root"}
+        </Typography.Title>
+        <Button
+          type="link"
+          shape="circle"
+          icon={<SettingOutlined />}
+          onClick={() => selectProperty({ schema: [], uiSchema: [] })}
+        />
+      </Row>
       <SchemaTree key="schemaTree" />
     </Col>
   );
---
+++ ui/cap-react/src/antd/admin/formComponents/widgets/index.js
@@ -0,0 +1,7 @@
+import SliderWidget from "./SliderWidget";
+
+const widgets = {
+  slider: SliderWidget,
+};
+
+export default widgets;
