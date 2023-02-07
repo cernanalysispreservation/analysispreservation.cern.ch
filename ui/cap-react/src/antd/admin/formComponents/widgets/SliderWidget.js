d38371d95cf7c5cb6681389a6186a22ade4006a9
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2023-02-07 18:52:14 +0100

    ui: fix bug when switching between fields #2720
    Signed-off-by: Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>

--- ui/cap-react/src/antd/admin/formComponents/widgets/SliderWidget.js
+++ ui/cap-react/src/antd/admin/formComponents/widgets/SliderWidget.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Slider } from "antd";
 
-const SliderWidget = ({ schema, onChange }) => {
+const SliderWidget = ({ schema, onChange, value }) => {
   const { defaultValue, values, labels } = schema;
 
   const [marks] = useState(() => {
@@ -18,7 +18,9 @@ const SliderWidget = ({ schema, onChange }) => {
   return (
     <Slider
       marks={marks}
-      defaultValue={defaultValue}
+      defaultValue={
+        values.indexOf(value) >= 0 ? values.indexOf(value) : defaultValue
+      }
       step={null}
       min={0}
       max={Object.keys(marks).length - 1}
@@ -31,6 +33,7 @@ const SliderWidget = ({ schema, onChange }) => {
 SliderWidget.propTypes = {
   schema: PropTypes.object,
   onChange: PropTypes.func,
+  value: PropTypes.number,
 };
 
 export default SliderWidget;
