94fcb9874e0437f3846e4de626065c1d6ef8c6df
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2023-02-28 17:51:58 +0100

    ui: change maskedinput description to markdown
    Signed-off-by: Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>

--- ui/cap-react/src/antd/admin/utils/fieldTypes.js
+++ ui/cap-react/src/antd/admin/utils/fieldTypes.js
@@ -109,32 +109,12 @@ const collections = {
               title: "Do you want this field to be hidden?",
               description: "If yes, this field will not be visible in the form",
             },
-            mask: {
-              type: "string",
-              title: "Input mask",
-              description: (
-                <div>
-                  Add a mask to visualize and limit the format of the input. Use the
-                  following format: <strong>'0'</strong> (number),{" "}
-                  <strong>'a'</strong> (lowercase letter), <strong>'A'</strong>{" "}
-                  (uppercase letter), <strong>'*'</strong> (letter or number). You
-                  can escape all these with <strong>'\'</strong>. The rest of the
-                  characters will be treated as constants
-                </div>
-              ),
-            },
           },
         },
       },
     },
     optionsUiSchemaUiSchema: {
       ...common.optionsUiSchemaUiSchema,
-      "ui:options": {
-        ...common.optionsUiSchemaUiSchema["ui:options"],
-        mask: {
-          "ui:placeholder": "BN-000/aa",
-        },
-      },
     },
     default: {
       schema: {
@@ -279,20 +259,6 @@ const simple = {
           type: "string",
           pattern: "^^[w]*$$",
         },
-        mask: {
-          type: "string",
-          title: "Input mask",
-          description: (
-            <div>
-              Add a mask to visualize and limit the format of the input. Use the
-              following format: <strong>'0'</strong> (number),{" "}
-              <strong>'a'</strong> (lowercase letter), <strong>'A'</strong>{" "}
-              (uppercase letter), <strong>'*'</strong> (letter or number). You
-              can escape all these with <strong>'\'</strong>. The rest of the
-              characters will be treated as constants
-            </div>
-          ),
-        },
         readOnly: extra.optionsSchema.readOnly,
       },
     },
@@ -300,9 +266,6 @@ const simple = {
       readOnly: extra.optionsSchemaUiSchema.readOnly,
       pattern: {
         "ui:placeholder": "^.*$",
-      },
-      mask: {
-        "ui:placeholder": "BN-000/aa",
       },
     },
     optionsUiSchema: {
