ae215ff679d1c4f774b24761e56ffb680003684d
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2023-03-31 13:21:42 +0200

    ui: upgraded to webpack 6 and react 18
    Signed-off-by: Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>

--- ui/cap-react/src/index.js
+++ ui/cap-react/src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/default */
 
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { AppContainer } from "react-hot-loader";
 import store, { history } from "./store/configureStore";
 import Root from "./antd/Root";
@@ -13,21 +13,11 @@ if (process.env.SENTRY_UI_DSN) {
   Sentry.init({ dsn: process.env.SENTRY_UI_DSN });
 }
 
-render(
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+root.render(
   <AppContainer>
     <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById("app")
-);
-
-if (module.hot) {
-  module.hot.accept("./antd/Root", () => {
-    const NewRoot = require("./antd/Root").default;
-    render(
-      <AppContainer>
-        <NewRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById("app")
-    );
-  });
-}
+  </AppContainer>
+)
