8eb002dc675008dc01152d3b6dfd6f649b16728f
pamfilos <pamfilosf@gmail.com>
2023-06-28 15:39:41 +0200

    ui: apiClient to intercept on ENABLE_BACKEND_PROXY
    Signed-off-by: pamfilos <pamfilosf@gmail.com>

--- ui/cap-react/src/axios.js
+++ ui/cap-react/src/axios.js
@@ -2,15 +2,17 @@ import axios from 'axios';
 
 const apiClient = axios.create();
 
-// Request interceptor to change URL
-apiClient.interceptors.request.use(config => {
-  if (config.url.startsWith('http://localhost:5000/api')) {
-    // Change the URL to relative
-    config.url = config.url.replace('http://localhost:5000', '');
-  }
-  return config;
-}, error => {
-  return Promise.reject(error);
-});
+if (process.env.ENABLE_BACKEND_PROXY) {
+  // Request interceptor to change URL
+  apiClient.interceptors.request.use(config => {
+    if (config.url.startsWith('http://localhost:5000/api')) {
+      // Change the URL to relative
+      config.url = config.url.replace('http://localhost:5000', '');
+    }
+    return config;
+  }, error => {
+    return Promise.reject(error);
+  });
+}
 
 export default apiClient;
