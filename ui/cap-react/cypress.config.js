3e3d5fd4c2ee4754566fa97de40dbd804f455ba2
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2024-07-19 13:16:55 +0200

    ui: add e2e test for save/publish draft validation
    Signed-off-by: Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>

--- ui/cap-react/cypress.config.js
+++ ui/cap-react/cypress.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   projectId: "4mpnou",
   video: false,
   downloadsFolder: "cypress/downloads",
